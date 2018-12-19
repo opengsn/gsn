1887671c6e9198cbcf1605320694090376c71af7
Alex Forshtat <alex@tabookey.com>
2018-12-19 20:18:46 +0200

    Test that stake works correctly when owner and relay are different addresses
    Relay should normally generate a new address itself on the first run,
    and only receive some ether to pay for the gas. It should not export a
    key or perform stake on it's own.

--- test/relay_contract.js
+++ test/relay_contract.js
@@ -50,6 +50,18 @@ contract("RelayHub", function (accounts) {
         sig = await getTransactionSignature(accounts[0], digest)
         let deposit = 100000000000;
         await sr.deposit({value: deposit});
+        let promise = new Promise(function (resolve, reject) {
+            new web3.providers.HttpProvider("http://localhost:8090/getaddr").sendAsync({}, function(error, response){
+                if (error){ 
+                    reject(error)
+                }
+                else {
+                    resolve(response.RelayServerAddress)
+                }
+            })
+        })
+        let addr = await promise
+        web3.eth.sendTransaction({to:addr, from:accounts[0], value:web3.toWei("0.5", "ether")})
     });
 
     var real_sender = accounts[0];
@@ -68,6 +80,22 @@ contract("RelayHub", function (accounts) {
         assert.equal(expected_stake, stake[0].toNumber()-z);
         assert.equal(7, stake[1]);
     });
+
+    it("should allow owner to stake on behalf of the relay", async function () {
+        let gasless_relay_address = "0x2Dd8C0665327A26D7655055B22c9b3bA596DfeD9"
+        let balance_of_gasless_before = web3.eth.getBalance(gasless_relay_address);
+        let balance_of_acc7_before = web3.eth.getBalance(accounts[7]);
+        let expected_stake = web3.toWei('0.5', 'ether')
+        let gasPrice = 1
+        let res = await rhub.stake(gasless_relay_address, 7, {value: expected_stake, gasPrice: gasPrice, from: accounts[7]})
+        let stake = await rhub.stakes(gasless_relay_address)
+        let balance_of_gasless_after = web3.eth.getBalance(gasless_relay_address);
+        let balance_of_acc7_after = web3.eth.getBalance(accounts[7]);
+        let expected_balance_after = balance_of_acc7_before.minus(expected_stake).minus(res.receipt.gasUsed * gasPrice)
+        assert.equal(balance_of_acc7_after.toString(), expected_balance_after.toString());
+        assert.equal(balance_of_gasless_after.toString(), balance_of_gasless_before.toString());
+        assert.equal(expected_stake, stake[0].toNumber());
+    })
 
     it("test_register_relay", async function () {
         let res = await register_new_relay(rhub, one_ether, dayInSec, 120, "hello", accounts[0]);
