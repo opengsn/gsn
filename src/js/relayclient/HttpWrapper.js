d5feb4841eb0167770bf169c97a21e3ece0d8752
Dror Tirosh <dror@tabookey.com>
2019-01-06 00:35:38 +0200

    Add timeout for HttpWrapper
    timeout request after 4 (default) seconds

--- src/js/relayclient/HttpWrapper.js
+++ src/js/relayclient/HttpWrapper.js
@@ -3,8 +3,9 @@ var unique_id = 1
 
 class HttpWrapper {
 
-    constructor(web3) {
+    constructor(web3, timeout) {
         this.web3 = web3
+        this.timeout = timeout || 5000
     }
 
     send(url, jsonRequestData, callback) {
@@ -15,7 +16,15 @@ class HttpWrapper {
             console.log("sending request:", localid, url, JSON.stringify(jsonRequestData).slice(0, 80))
         }
 
+        let req = { done:false }
         let callback1 = function (e, r) {
+            if ( this.done ) {
+                console.log("already done")
+                return
+            }
+            clearTimeout( this.timeoutId )
+            this.done=true
+
             if (e && ("" + e).indexOf("Invalid JSON RPC response") >= 0) {
                 e = { error: "invalid-json" }
             }
@@ -26,7 +35,10 @@ class HttpWrapper {
                 console.log("got response:", localid, JSON.stringify(r).slice(0, 80), "err=", JSON.stringify(e).slice(0, 80))
             }
             callback(e, r)
-        }
+        }.bind(req)
+
+        req.timeoutId = setTimeout(() => { callback1("timed-out: "+url,null) }, this.timeout)
+
         new this.web3.providers.HttpProvider(url).sendAsync(jsonRequestData, callback1);
     }
 
