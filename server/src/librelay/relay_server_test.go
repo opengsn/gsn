c7ae94a927c598a58b55251b3e28688fbc78d3a9
Alex Forshtat <alex@tabookey.com>
2019-01-07 16:39:11 +0200

    Initial support for testing with Simulated Backend
    Instead of running ganache for Golang test, go-ethereum provides a
    simulated backend implementation.
    There is some difference in implemented interfaces, and 'IClient' is
    added as a minimal set of used methods.
    Test for 'RefreshGasPrice' method as a sample.

--- server/src/librelay/relay_server_test.go
+++ server/src/librelay/relay_server_test.go
@@ -1,17 +1,15 @@
 package librelay
 
 import (
-	"encoding/json"
-	"fmt"
-	"gen/librelay"
+	"context"
+	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"log"
 	"math/big"
-	"os/exec"
 	"testing"
 )
 
@@ -26,111 +24,70 @@ const ethereumNodeURL = "http://localhost:8545"
 
 var relayHubAddress = common.HexToAddress("0x254dffcd3277c0b1660f6d42efbb754edababc2b") //0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab
 
-func TestRelayServer(t *testing.T) {
-
-	fmt.Println("relay server test")
-
-	if runGanache {
-		err := exec.Command("ganache-cli", "-d").Start()
-		if err != nil {
-			log.Fatalln("Error running ganache-cli", err)
-		}
-		fmt.Println("Running ganache-cli")
-	}
-
-	/*
-	* abigen seems to suffer the same issue of web3j - the generated bin of the contract contains unresolved libs and the api generated doesn't provide an option to link
-	* between contracts.
-	 */
-	if runTruffleMigrate {
-		err := exec.Command("truffle", "migrate").Run()
-		if err != nil {
-			log.Fatalln("Error running truffle migrate", err)
-		}
-		fmt.Println("Running truffle migrate")
-
-	} else {
-		// deploying RelayHub
-		key, _ := crypto.HexToECDSA("4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d")
-		//crypto.GenerateKey()
-		auth := bind.NewKeyedTransactor(key)
-		alloc := make(core.GenesisAlloc)
-		alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(133700000)}
-		//sim := backends.NewSimulatedBackend(alloc, 10000000)
-		client, err := ethclient.Dial(ethereumNodeURL)
-		if err != nil {
-			fmt.Println("Could not connect to ethereum node", err)
-			log.Fatal(err)
-		}
-		rlpaddr, _, _, err := librelay.DeployRLPReader(auth, client)
-		if err != nil {
-			log.Fatalf("could not deploy contract: %v", err)
-		}
-		// interact with contract
-		fmt.Printf("RLPReader library deployed to %s\n", rlpaddr.String())
-		// deploy contract
-		rhaddr, _, rhub, err := librelay.DeployRelayHub(auth, client)
-		if err != nil {
-			log.Fatalf("could not deploy contract: %v", err)
-		}
-		// interact with contract
-		fmt.Printf("RelayHub contract deployed to %s\n", rhaddr.String())
-		rhub.PenalizeRepeatedNonce(auth, nil, nil, nil, nil)
-	}
-
+type FakeClient struct {
+	*backends.SimulatedBackend
 }
 
-// TODO: hardcode known requests to test CreateRelayTransaction()
-func TestRelayServer_CreateRelayTransaction(t *testing.T) {
-
-	ownerAddress := common.HexToAddress("0x610bb1573d1046fcb8a70bbbd395754cd57c2b60")
-	fee := big.NewInt(11)
-	port := 8090
-	url := "http://localhost:" + string(port)
-	stakeAmount := big.NewInt(1000)
-	gasLimit := uint64(100000)
-	gasPrice := big.NewInt(100)
-	privateKey, err := crypto.HexToECDSA("77c5495fbb039eed474fc940f29955ed0531693cc9212911efd35dff0373153f")
-	if err != nil {
-		log.Fatal(err)
-	}
-	unstakeDelay := big.NewInt(10)
-	fmt.Println("Constructing relay server")
-	var relay IRelay = &RelayServer{ownerAddress, fee, url, port,
-		relayHubAddress, stakeAmount,
-		gasLimit, gasPrice, privateKey, unstakeDelay, ethereumNodeURL}
-	fmt.Println("Staking...")
-	err = relay.Stake()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("1Done staking")
-
-	fmt.Println("1Registering relay...")
-	err = relay.RegisterRelay(common.HexToAddress("0"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Done registering")
-
-	body := []byte("{\"encodedFunction\": \"1\", \"signature\": [0,0,0,1,2,3], \"from\": \"0xa1b1c3d4e5a1b1c3d4e5a1b1c3d4e5a1b1c3d4e5\", " +
-		"\"to\": \"0xb1b1c3d4e5a1b1c3d4e5a1b1c3d4e5a1b1c3d4e5\", \"gasPrice\":  1, \"gasLimit\": 1, \"recipientNonce\": 1, \"relayFee\":12}")
-	var request = &RelayTransactionRequest{}
-	err = json.Unmarshal(body, request)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Println("encodedFunction: " + request.EncodedFunction)
-	fmt.Println("From: " + request.From.String())
-	fmt.Println("To: " + request.To.String())
-	fmt.Println("GasPrice: " + request.GasPrice.String())
-	fmt.Println("GasLimit: " + request.GasLimit.String())
-	fmt.Println("RecipientNonce: " + request.RecipientNonce.String())
-	fmt.Println("RelayFee: " + request.RelayFee.String())
-	fmt.Println("Signature: ", request.Signature)
-
-	// TODO: create valid request
-	//signedTx := relay.CreateRelayTransaction(*request)
-
+func (client *FakeClient) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
+	return &types.Block{}, nil
 }
+
+func (client *FakeClient) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
+	return &types.Header{}, nil
+}
+
+func (client *FakeClient) TransactionByHash(ctx context.Context, txHash common.Hash) (tx *types.Transaction, isPending bool, err error) {
+	return &types.Transaction{}, false, nil
+}
+
+func NewRelaySimBackend(t *testing.T) (relay IRelay, sim *FakeClient) {
+	alloc := make(core.GenesisAlloc)
+	key, _ := crypto.HexToECDSA("4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d")
+	auth := bind.NewKeyedTransactor(key)
+	alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(133700000)}
+	sim = &FakeClient{}
+	sim.SimulatedBackend = backends.NewSimulatedBackend(alloc, 10000000)
+
+
+	fee := &big.Int{}
+	stakeAmount := &big.Int{}
+	gasLimit := uint64(0)
+	gasPricePercent := &big.Int{}
+	url := ""
+	port := 0
+	relayHubAddress := common.Address{}
+	privateKey := &ecdsa.PrivateKey{}
+	unstakeDelay := &big.Int{}
+	ethereumNodeUrl := ""
+
+	relay, err := NewRelayServer(
+		common.Address{}, fee, url, port,
+		relayHubAddress, stakeAmount, gasLimit,
+		gasPricePercent, privateKey, unstakeDelay,
+		ethereumNodeUrl, sim)
+	if err != nil {
+		t.Error("Relay was not created", err)
+	}
+	return
+}
+
+func ErrFail(err error, t *testing.T){
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRefreshGasPrice(t *testing.T) {
+	relay, _ := NewRelaySimBackend(t)
+	gasPriceBefore := relay.GasPrice()
+	ErrFail(relay.RefreshGasPrice(), t)
+	gasPriceAfter := relay.GasPrice()
+	if gasPriceBefore.Cmp(big.NewInt(0)) != 0 {
+		t.Error()
+	}
+	if gasPriceAfter.Cmp(big.NewInt(1)) != 0 {
+		t.Error()
+	}
+}
+
+
