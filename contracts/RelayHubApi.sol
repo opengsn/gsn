a6496f307e8d2fd2a80e9acbb883e03a16c23565
Dror Tirosh <dror@tabookey.com>
2019-01-09 00:57:08 +0200

    Fix event "indexed"
    relay address should always be indexed.
    
    TransactionRelayed should be indexed by from, not by hash

--- contracts/RelayHubApi.sol
+++ contracts/RelayHubApi.sol
@@ -2,20 +2,20 @@ pragma solidity ^0.4.18;
 
 contract  RelayHubApi {
 
-    event Staked(address relay, uint stake);
-    event Unstaked(address relay, uint stake);
+    event Staked(address indexed relay, uint stake);
+    event Unstaked(address indexed relay, uint stake);
     /*
     As per https://github.com/ethereum/go-ethereum/pull/16513; https://github.com/ethereum/go-ethereum/issues/15710
     any abi fields with underscore are changed to camelCase by abigen, so in order to parse the correct fields with go
     we change the field names to camelCase where we need them in go
     */
-    event RelayAdded(address relay, uint transactionFee, uint stake, uint unstakeDelay, string url);  // relay not indexed, that's how apps learn of new relays
-    event RelayRemoved(address indexed relay, uint unstake_time);  // relay is indexed, as apps need to watch for removal of relays in their pool.
+    event RelayAdded(address indexed relay, uint transactionFee, uint stake, uint unstakeDelay, string url);
+    event RelayRemoved(address indexed relay, uint unstake_time);
     event NeedsFunding(address indexed relay);
-    event TransactionRelayed(address indexed relay, bytes32 indexed hash, address from, bool success, uint charge);
+    event TransactionRelayed(address indexed relay, address indexed from, bytes32 hash, bool success, uint charge);
     event Deposited(address src, uint amount);
     event Withdrawn(address dest, uint amount);
-    event Penalized(address relay, address sender, uint amount);
+    event Penalized(address indexed relay, address sender, uint amount);
 
     function get_nonce(address from) view external returns (uint);
     function relay(address from, address to, bytes transaction_orig, uint transaction_fee, uint gas_price, uint gas_limit, uint nonce, bytes sig) public;
