465f33008448969e05ccf6d6e8ca9f8098632ff7
Dror Tirosh <dror@tabookey.com>
2019-01-23 10:40:00 +0200

    support multiple solc versions
    make the contract compatible with solc 0.4.00 .. 0.5.99

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.0 <0.6.0;
 
 contract Migrations {
   address public owner;
--- contracts/SampleRecipient.sol
+++ contracts/SampleRecipient.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.0 <0.6.0;
 
 import "./RelayHub.sol";
 import "./RelayRecipient.sol";
@@ -44,7 +44,7 @@ contract SampleRecipient is RelayRecipient {
         blacklisted = addr;
     }
 
-    function accept_relayed_call(address relay, address from, bytes calldata /*encoded_function*/, uint /*gas_price*/, uint /*transaction_fee*/ ) external view returns(uint32) {
+    function accept_relayed_call(address relay, address from, bytes memory /*encoded_function*/, uint /*gas_price*/, uint /*transaction_fee*/ ) public view returns(uint32) {
         // The factory accepts relayed transactions from anyone, so we whitelist our own relays to prevent abuse.
         // This protection only makes sense for contracts accepting anonymous calls, and therefore not used by Gatekeeper or Multisig.
         // May be protected by a user_credits map managed by a captcha-protected web app or association with a google account.
@@ -56,7 +56,7 @@ contract SampleRecipient is RelayRecipient {
 
     event SampleRecipientPostCall(uint used_gas );
 
-    function post_relayed_call(address /*relay*/ , address /*from*/, bytes calldata /*encoded_function*/, bool /*success*/, uint used_gas, uint transaction_fee ) external {
+    function post_relayed_call(address /*relay*/ , address /*from*/, bytes memory /*encoded_function*/, bool /*success*/, uint used_gas, uint transaction_fee ) public {
 
         emit SampleRecipientPostCall(used_gas * tx.gasprice * (transaction_fee+100)/100);
     }
