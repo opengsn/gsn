e0c33baa70f6015d2c895054285d13c845b99009
Alex Forshtat <alex@tabookey.com>
2019-01-27 18:37:22 +0200

    Create 'publish_server_docker' script to automate publishing

---
+++ publish_server_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+repository=tabookey/gsn-dev-server
+version=`docker run -t gsn-dev-server cat version`
+
+regex='^[^ -]+$'
+if [[ ! $version =~ $regex ]];
+then
+	echo "FATAL: ${version} contains illegal characters"
+	exit 1
+fi
+
+docker tag gsn-dev-server $repository:$version
+docker push $repository:$version
