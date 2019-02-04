615a360d2da20b8663a8ce0424a94683ec0f4d03
Alex Forshtat <alex@tabookey.com>
2019-02-04 16:06:22 +0200

    Add 'xDai POA Mainnet' to truffle.js
    Note: actual money are used in this network.
    Mnemonic is git-ignored.

--- truffle.js
+++ truffle.js
@@ -1,6 +1,8 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = "digital unknown jealous mother legal hedgehog save glory december universe spread figure custom found six"
 
+const fs=require('fs')
+let secret_mnemonic = fs.readFileSync( "./secret_mnemonic", {encoding:'utf8'} )
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -27,6 +29,13 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/c3422181d0594697a38defe7706a1e5b")
       },
       network_id: 3
+    },
+    xdai_poa_mainnet: {
+      provider: function() {
+        let wallet = new HDWalletProvider(secret_mnemonic, "https://dai.poa.network")
+        return wallet
+      },
+      network_id: 100
     }
   },
   mocha: {
