039a2e5e6c4eee4f16bf99f544e48d7a483e2763
Alex Forshtat <alex@tabookey.com>
2019-02-17 19:38:12 +0200

    GLS-50 In RelayHub, verify that relays are not contracts (#51)
    * In RelayHub, verify that relays are not contracts
    
    There is no legitimate reason for a relay address to be controlled by a
    smart contract within our design of Relay Network.
    Making sure that message sender is the transaction origin is the safest
    way to validate the caller is externally-owned.
    
    * Separate positive and negative 'register' tests

--- contracts/TestRecipientUtils.sol
+++ contracts/TestRecipientUtils.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.4.0 <0.6.0;
 
 import './RecipientUtils.sol';
+import './RelayHub.sol';
 
 contract TestRecipientUtils is RecipientUtils {
 
@@ -9,4 +10,9 @@ contract TestRecipientUtils is RecipientUtils {
         emit Unused();  //just to avoid warnings..
     }
 
+    function registerAsRelay(RelayHub relayhub) public payable {
+        relayhub.stake.value(msg.value)(address(this), 1);
+        relayhub.register_relay(10, "string memory url", address(0));
+    }
+    function() external payable {}
 }
