6d78aba5209a163dfed80960b349c65ac51cbc97
Santiago Palladino <spalladino@gmail.com>
2019-04-08 16:08:43 -0300

    Use axios for http requests

--- src/js/relayclient/HttpWrapper.js
+++ src/js/relayclient/HttpWrapper.js
@@ -1,52 +1,41 @@
-var logreq = process.env.httpsendlog
-var unique_id = 1
+const axios = require('axios');
+const logreq = process.env.httpsendlog;
+const logmaxlen = 120
 
 class HttpWrapper {
 
-    constructor(web3) {
-        this.HttpProvider = web3.providers.HttpProvider
+    constructor(opts) {
+        this.provider = axios.create(Object.assign({
+            headers: { 'Content-Type': 'application/json' }
+        }, opts));
+
+        if (logreq) {
+            this.provider.interceptors.response.use(function (response) {
+                console.log("got response:", response.config.url, JSON.stringify(response.data).slice(0, logmaxlen))
+                return response;
+            }, function (error) {
+                const errData = error.response ? error.response.data : { error: error.message };
+                const errStr = ((typeof errData === 'string') ? errData : JSON.stringify(errData)).slice(0, logmaxlen);
+                const errUrl = error.response ? error.response.config.url : error.address;
+                console.log("got response:", errUrl, "err=", errStr);
+                return Promise.reject(error);
+            });
+        }
     }
 
     send(url, jsonRequestData, callback) {
-        let maxlen = 120
-
-        jsonRequestData = jsonRequestData || {}
-        let localid = unique_id++
-        if (logreq) {
-            console.log("sending request:", localid, url, JSON.stringify(jsonRequestData).slice(0, maxlen))
-        }
-
-        let callback1 = function (e, r) {
-            if (e && ("" + e).indexOf("Invalid JSON RPC response") >= 0) {
-                e = {error: "invalid-json"}
-            }
-            if (("" + r).indexOf("\"error\"") >= 0) {
-                e = r;
-                r = null;
-            }
-            if (!e && r && r.error) {
-                e = r;
-                r = null
-            }
-            if (logreq) {
-                console.log("got response:", localid, JSON.stringify(r).slice(0, maxlen), "err=", JSON.stringify(e).slice(0, maxlen))
-            }
-            callback(e, r)
-        }
-
-        let provider = new this.HttpProvider(url)
-        let send = provider['sendAsync'] || provider['send']
-        send.bind(provider)(jsonRequestData, callback1);
+        this.sendPromise(url, jsonRequestData || {})
+            .then(data => callback(null, data))
+            .catch(err => callback(err, null));
     }
 
     sendPromise(url, jsonRequestData) {
-        let self = this
-        return new Promise((resolve, reject) => {
-            self.send(url, jsonRequestData, (e, r) => {
-                if (e) return reject(e)
-                return resolve(r)
-            })
-        })
+        if (logreq) {
+            console.log("sending request:", url, JSON.stringify(jsonRequestData || {}).slice(0, logmaxlen))
+        }
+        return this.provider.post(url, jsonRequestData)
+            .then(res => res.data)
+            .catch(err => Promise.reject(err.response ? err.response.data : { error: err.message }));
     }
 }
 
--- test/http_wrapper_test.js
+++ test/http_wrapper_test.js
@@ -5,7 +5,7 @@ const Web3 = require('web3')
 
 describe("HttpWrapper", () => {
     it("global web3: connect to node, get version", async () => {
-        let http = new HttpWrapper(web3);
+        let http = new HttpWrapper();
 
         let res = await new Promise((resolve) => {
             let url = web3.currentProvider.host
@@ -18,19 +18,20 @@ describe("HttpWrapper", () => {
 
         assert.equal( 123, res.id, JSON.stringify(res) ) //just verify its a valid response
     })
+    
     it("global web3: should fail on connection refused", async () => {
-        let http = new HttpWrapper(web3);
+        let http = new HttpWrapper();
         let res = await new Promise((resolve) => {
             http.send("http://localhost:44321", {jsonrpc: "2.0", method: "net_version", id:123}, (e, r) => {
                 if (e) resolve( "err: "+JSON.stringify(e))
                 resolve(r)
             })
         })
-        assert.equal( "err: {\"error\":\"invalid-json\"}", res)
+        assert.equal( "err: {\"error\":\"connect ECONNREFUSED 127.0.0.1:44321\"}", res)
     })
 
     it("loaded Web3: connect to node, get version", async () => {
-        let http = new HttpWrapper(Web3);
+        let http = new HttpWrapper();
 
         let res = await new Promise((resolve) => {
             let url = web3.currentProvider.host
@@ -42,14 +43,15 @@ describe("HttpWrapper", () => {
 
         assert.equal( 123, res.id, JSON.stringify(res) ) //just verify its a valid response
     })
+    
     it("loaded Web3: should fail on connection refused", async () => {
-        let http = new HttpWrapper(Web3);
+        let http = new HttpWrapper();
         let res = await new Promise((resolve) => {
             http.send("http://localhost:44321", {jsonrpc: "2.0", method: "net_version", id:123}, (e, r) => {
                 if (e) resolve( "err: "+JSON.stringify(e))
                 resolve(r)
             })
         })
-        assert.equal( "err: {\"error\":\"invalid-json\"}", res)
+        assert.equal( "err: {\"error\":\"connect ECONNREFUSED 127.0.0.1:44321\"}", res)
     })
 })
