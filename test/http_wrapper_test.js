da8955053058bf75b66cf1cf6220d4b1aa6a2ef8
Santiago Palladino <spalladino@gmail.com>
2019-04-08 17:37:35 -0300

    Add test for http timeout

--- test/http_wrapper_test.js
+++ test/http_wrapper_test.js
@@ -1,6 +1,9 @@
+const assert = require('chai')
+  .use(require('chai-as-promised'))
+  .assert;
+
 /* global describe web3 require assert it */
 const HttpWrapper = require('../src/js/relayclient/HttpWrapper')
-
 const Web3 = require('web3')
 
 describe("HttpWrapper", () => {
@@ -27,5 +30,19 @@ describe("HttpWrapper", () => {
             })
         })
         assert.equal( "err: {\"error\":\"connect ECONNREFUSED 127.0.0.1:44321\"}", res)
+    })
+
+    it("should timeout after specified time", async () => {
+        // this test abuses the fact that a local ganache is slow, and should take over 1ms to respond even if it's local
+        const http = new HttpWrapper({ timeout: 1 });
+        let error = null;
+        await assert.isRejected(
+            http.sendPromise(web3.currentProvider.host, {jsonrpc: "2.0", method: "net_version", id:123})
+                .catch(err => {
+                    error = err;
+                    return Promise.reject(err);
+                })
+        );
+        assert.deepEqual(error, { error: 'timeout of 1ms exceeded' });
     })
 })
