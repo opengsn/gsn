736e1323dad360451376c0f4c64e6b9bed3aef28
Alex Forshtat <alex@tabookey.com>
2019-04-24 18:22:51 +0300

    Update docker image used for CircleCI tests

--- scripts/solc-abigen-wrapper.sh
+++ scripts/solc-abigen-wrapper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 rootdir=$(cd $(dirname $(dirname $0));pwd)
 allowedpath=${rootdir}/node_modules/
+# we are not using 'npx solc' (aka solcjs) because it sucks and doesn't work (API difference)
 solc openzeppelin-solidity=${allowedpath}/openzeppelin-solidity \@0x=${allowedpath}/\@0x $*
