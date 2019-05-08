1734c4aab6dff79d1a12d6cce496a492b35eae61
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-05-08 16:30:18 -0300

    Move accept_relayed_call and post_relayed_call into RelayRecipientAPI.

--- contracts/RelayRecipient.sol
+++ contracts/RelayRecipient.sol
@@ -30,7 +30,7 @@ contract RelayRecipient is RelayRecipientApi {
         require(relay_hub == RelayHub(0), "init_relay_hub: rhub already set");
         set_relay_hub(_rhub);
     }
-    
+
     function set_relay_hub(RelayHub _rhub) internal {
         // Normally called just once, during init_relay_hub.
         // Left as a separate internal function, in case a contract wishes to have its own update mechanism for RelayHub.
@@ -79,32 +79,5 @@ contract RelayRecipient is RelayRecipientApi {
         }
         return orig_msg_data;
     }
-
-    /*
-	 * Contract must inherit and re-implement this method.
-	 *  @return "0" if the the contract is willing to accept the charges from this sender, for this function call.
-	 *  	any other value is a failure. actual value is for diagnostics only.
-	 *** Note :values below 10 are reserved by can_relay
-	 *  @param relay the relay that attempts to relay this function call.
-	 * 			the contract may restrict some encoded functions to specific known relays.
-	 *  @param from the sender (signer) of this function call.
-	 *  @param encoded_function the encoded function call (without any ethereum signature).
-	 * 			the contract may check the method-id for valid methods
-	 *  @param gas_price - the gas price for this transaction
-	 *  @param transaction_fee - the relay compensation (in %) for this transaction
-	 *  @param approval - first 65 bytes are checked by the RelayHub and reserved for the sender's signature, and the rest is
-     *           available for dapps in their specific use-cases
-	 */
-    function accept_relayed_call(address relay, address from, bytes memory encoded_function, uint gas_price, uint transaction_fee, bytes memory approval) public view returns(uint32);
-
-    /**
-     * This method is called after the relayed call.
-     * It may be used to record the transaction (e.g. charge the caller by some contract logic) for this call.
-     * the method is given all parameters of accept_relayed_call, and also the success/failure status and actual used gas.
-     * - success - true if the relayed call succeeded, false if it reverted
-     * - used_gas - gas used up to this point. Note that gas calculation (for the purpose of compensation
-     *   to the relay) is done after this method returns.
-     */
-    function post_relayed_call(address relay, address from, bytes memory encoded_function, bool success, uint used_gas, uint transaction_fee ) public;
 }
 
--- contracts/RelayRecipientApi.sol
+++ contracts/RelayRecipientApi.sol
@@ -13,4 +13,30 @@ contract RelayRecipientApi {
      * before making any.
      */
     function get_recipient_balance() public view returns (uint);
+
+    /*
+     *  @return "0" if the the contract is willing to accept the charges from this sender, for this function call.
+     *      any other value is a failure. actual value is for diagnostics only.
+     *** Note :values below 10 are reserved by can_relay
+     *  @param relay the relay that attempts to relay this function call.
+     *          the contract may restrict some encoded functions to specific known relays.
+     *  @param from the sender (signer) of this function call.
+     *  @param encoded_function the encoded function call (without any ethereum signature).
+     *          the contract may check the method-id for valid methods
+     *  @param gas_price - the gas price for this transaction
+     *  @param transaction_fee - the relay compensation (in %) for this transaction
+     *  @param approval - first 65 bytes are checked by the RelayHub and reserved for the sender's signature, and the rest is
+     *           available for dapps in their specific use-cases
+     */
+    function accept_relayed_call(address relay, address from, bytes memory encoded_function, uint gas_price, uint transaction_fee, bytes memory approval) public view returns (uint32);
+
+    /**
+     * This method is called after the relayed call.
+     * It may be used to record the transaction (e.g. charge the caller by some contract logic) for this call.
+     * the method is given all parameters of accept_relayed_call, and also the success/failure status and actual used gas.
+     * - success - true if the relayed call succeeded, false if it reverted
+     * - used_gas - gas used up to this point. Note that gas calculation (for the purpose of compensation
+     *   to the relay) is done after this method returns.
+     */
+    function post_relayed_call(address relay, address from, bytes memory encoded_function, bool success, uint used_gas, uint transaction_fee) public;
 }
