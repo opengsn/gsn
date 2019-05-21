blockarraygroup/tabookey-gasless

Refactoring snake_case to camelCase

contracts/RelayRecipient.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.4.0 <0.6.0;
 //
 // The recipient contract is responsible to:
 // * pass a trusted RelayHub singleton to the constructor.
-// * Implement accept_relayed_call, which acts as a whitelist/blacklist of senders.  It is advised that the recipient's owner will be able to update that list to remove abusers.
-// * In every function that cares about the sender, use "address sender = get_sender()" instead of msg.sender.  It'll return msg.sender for non-relayed transactions, or the real sender in case of relayed transactions.
+// * Implement acceptRelayedCall, which acts as a whitelist/blacklist of senders.  It is advised that the recipient's owner will be able to update that list to remove abusers.
+// * In every function that cares about the sender, use "address sender = getSender()" instead of msg.sender.  It'll return msg.sender for non-relayed transactions, or the real sender in case of relayed transactions.
 
 import "./RelayRecipientApi.sol";
 import "./RelayHub.sol";
@@ -13,10 +13,10 @@ import "@0x/contracts-utils/contracts/src/LibBytes.sol";
 
 contract RelayRecipient is RelayRecipientApi {
 
-    RelayHub private relay_hub; // The RelayHub singleton which is allowed to call us
+    RelayHub private relayHub; // The RelayHub singleton which is allowed to call us
 
-	function get_hub_addr() public view returns (address) {
-		return address(relay_hub);
+	function getHubAddr() public view returns (address) {
+		return address(relayHub);
 	}
 
     /**
@@ -26,85 +26,85 @@ contract RelayRecipient is RelayRecipientApi {
      * This method might also be called by the owner, in order to use a new RelayHub - since the RelayHub
      * itself is not an upgradable contract.
      */
-    function init_relay_hub(RelayHub _rhub) internal {
-        require(relay_hub == RelayHub(0), "init_relay_hub: rhub already set");
-        set_relay_hub(_rhub);
+    function initRelayHub(RelayHub _rhub) internal {
+        require(relayHub == RelayHub(0), "initRelayHub: rhub already set");
+        setRelayHub(_rhub);
     }
     
-    function set_relay_hub(RelayHub _rhub) internal {
-        // Normally called just once, during init_relay_hub.
+    function setRelayHub(RelayHub _rhub) internal {
+        // Normally called just once, during initRelayHub.
         // Left as a separate internal function, in case a contract wishes to have its own update mechanism for RelayHub.
-        relay_hub = _rhub;
+        relayHub = _rhub;
 
         //attempt a read method, just to validate the relay is a valid RelayHub contract.
-        get_recipient_balance();
+        getRecipientBalance();
     }
 
-    function get_relay_hub() internal view returns (RelayHub) {
-        return relay_hub;
+    function getRelayHub() internal view returns (RelayHub) {
+        return relayHub;
     }
 
     /**
      * return the balance of this contract.
      * Note that this method will revert on configuration error (invalid relay address)
      */
-    function get_recipient_balance() public view returns (uint) {
-        return get_relay_hub().balanceOf(address(this));
+    function getRecipientBalance() public view returns (uint) {
+        return getRelayHub().balanceOf(address(this));
     }
 
-    function get_sender_from_data(address orig_sender, bytes memory msg_data) public view returns(address) {
-        address sender = orig_sender;
-        if (orig_sender == get_hub_addr() ) {
+    function getSenderFromData(address origSender, bytes memory msgData) public view returns(address) {
+        address sender = origSender;
+        if (origSender == getHubAddr() ) {
             // At this point we know that the sender is a trusted RelayHub, so we trust that the last bytes of msg.data are the verified sender address.
             // extract sender address from the end of msg.data
-            sender = LibBytes.readAddress(msg_data, msg_data.length - 20);
+            sender = LibBytes.readAddress(msgData, msgData.length - 20);
         }
         return sender;
     }
 
-    function get_sender() public view returns(address) {
-        return get_sender_from_data(msg.sender, msg.data);
+    function getSender() public view returns(address) {
+        return getSenderFromData(msg.sender, msg.data);
     }
 
-    function get_message_data() public view returns(bytes memory) {
-        bytes memory orig_msg_data = msg.data;
-        if (msg.sender == get_hub_addr()) {
+    function getMessageData() public view returns(bytes memory) {
+        bytes memory origMsgData = msg.data;
+        if (msg.sender == getHubAddr()) {
             // At this point we know that the sender is a trusted RelayHub, so we trust that the last bytes of msg.data are the verified sender address.
             // extract original message data from the start of msg.data
-            orig_msg_data = new bytes(msg.data.length - 20);
-            for (uint256 i = 0; i < orig_msg_data.length; i++)
+            origMsgData = new bytes(msg.data.length - 20);
+            for (uint256 i = 0; i < origMsgData.length; i++)
             {
-                orig_msg_data[i] = msg.data[i];
+                origMsgData[i] = msg.data[i];
             }
         }
-        return orig_msg_data;
+        return origMsgData;
     }
 
     /*
 	 * Contract must inherit and re-implement this method.
 	 *  @return "0" if the the contract is willing to accept the charges from this sender, for this function call.
 	 *  	any other value is a failure. actual value is for diagnostics only.
-	 *** Note :values below 10 are reserved by can_relay
+	 *** Note :values below 10 are reserved by canRelay
 	 *  @param relay the relay that attempts to relay this function call.
 	 * 			the contract may restrict some encoded functions to specific known relays.
 	 *  @param from the sender (signer) of this function call.
-	 *  @param encoded_function the encoded function call (without any ethereum signature).
+	 *  @param encodedFunction the encoded function call (without any ethereum signature).
 	 * 			the contract may check the method-id for valid methods
-	 *  @param gas_price - the gas price for this transaction
-	 *  @param transaction_fee - the relay compensation (in %) for this transaction
+	 *  @param gasPrice - the gas price for this transaction
+	 *  @param transactionFee - the relay compensation (in %) for this transaction
 	 *  @param approval - first 65 bytes are checked by the RelayHub and reserved for the sender's signature, and the rest is
      *           available for dapps in their specific use-cases
 	 */
-    function accept_relayed_call(address relay, address from, bytes memory encoded_function, uint gas_price, uint transaction_fee, bytes memory approval) public view returns(uint32);
+    function acceptRelayedCall(address relay, address from, bytes memory encodedFunction, uint gasPrice, uint transactionFee, bytes memory approval) public view returns(uint32);
 
     /**
      * This method is called after the relayed call.
      * It may be used to record the transaction (e.g. charge the caller by some contract logic) for this call.
-     * the method is given all parameters of accept_relayed_call, and also the success/failure status and actual used gas.
+     * the method is given all parameters of acceptRelayedCall, and also the success/failure status and actual used gas.
      * - success - true if the relayed call succeeded, false if it reverted
-     * - used_gas - gas used up to this point. Note that gas calculation (for the purpose of compensation
+     * - usedGas - gas used up to this point. Note that gas calculation (for the purpose of compensation
      *   to the relay) is done after this method returns.
      */
-    function post_relayed_call(address relay, address from, bytes memory encoded_function, bool success, uint used_gas, uint transaction_fee ) public;
+    function postRelayedCall(address relay, address from, bytes memory encodedFunction, bool success, uint usedGas, uint transactionFee) public;
 }
 

contracts/SampleRecipient.sol
@@ -7,19 +7,19 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract SampleRecipient is RelayRecipient, Ownable {
 
-    mapping (address => bool) public relays_whitelist;
+    mapping (address => bool) public relaysWhitelist;
 
     constructor(RelayHub rhub) public {
-        init_relay_hub(rhub);
+        initRelayHub(rhub);
     }
 
     function deposit() public payable {
-        get_relay_hub().depositFor.value(msg.value)(address(this));
+        getRelayHub().depositFor.value(msg.value)(address(this));
     }
 
     function withdraw() public onlyOwner {
-        uint balance = get_relay_hub().balances(address(this));
-        get_relay_hub().withdraw(balance);
+        uint balance = getRelayHub().balances(address(this));
+        getRelayHub().withdraw(balance);
         msg.sender.transfer(balance);
     }
 
@@ -32,26 +32,26 @@ contract SampleRecipient is RelayRecipient, Ownable {
 
     function () external payable {}
 
-    event SampleRecipientEmitted(string message, address real_sender, address msg_sender, address origin);
+    event SampleRecipientEmitted(string message, address realSender, address msgSender, address origin);
     function emitMessage(string memory message) public {
-        emit SampleRecipientEmitted(message, get_sender(), msg.sender, tx.origin);
+        emit SampleRecipientEmitted(message, getSender(), msg.sender, tx.origin);
     }
 
-    function set_relay(address relay, bool on) public {
-        relays_whitelist[relay] = on;
+    function setRelay(address relay, bool on) public {
+        relaysWhitelist[relay] = on;
     }
 
     address public blacklisted;
 
-    function set_blacklisted(address addr) public {
+    function setBlacklisted(address addr) public {
         blacklisted = addr;
     }
 
-    function accept_relayed_call(address relay, address from, bytes memory /*encoded_function*/, uint /*gas_price*/, uint /*transaction_fee*/ , bytes memory approval) public view returns(uint32) {
+    function acceptRelayedCall(address relay, address from, bytes memory /*encodedFunction*/, uint /*gasPrice*/, uint /*transactionFee*/ , bytes memory approval) public view returns(uint32) {
         // The factory accepts relayed transactions from anyone, so we whitelist our own relays to prevent abuse.
         // This protection only makes sense for contracts accepting anonymous calls, and therefore not used by Gatekeeper or Multisig.
         // May be protected by a user_credits map managed by a captcha-protected web app or association with a google account.
-        if ( relays_whitelist[relay] ) return 0;
+        if ( relaysWhitelist[relay] ) return 0;
         if (from == blacklisted) return 11;
         
         // this is an example of how the dapp can provide an offchain approval to a transaction
@@ -69,11 +69,11 @@ contract SampleRecipient is RelayRecipient, Ownable {
 		return 0;
     }
 
-    event SampleRecipientPostCall(uint used_gas );
+    event SampleRecipientPostCall(uint usedGas );
 
-    function post_relayed_call(address /*relay*/ , address /*from*/, bytes memory /*encoded_function*/, bool /*success*/, uint used_gas, uint transaction_fee ) public {
+    function postRelayedCall(address /*relay*/ , address /*from*/, bytes memory /*encodedFunction*/, bool /*success*/, uint usedGas, uint transactionFee) public {
 
-        emit SampleRecipientPostCall(used_gas * tx.gasprice * (transaction_fee+100)/100);
+        emit SampleRecipientPostCall(usedGas * tx.gasprice * (transactionFee +100)/100);
     }
 
 }

opengsn/gsn

contracts/RelayRecipient.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.4.0 <0.6.0;
 //
 // The recipient contract is responsible to:
 // * pass a trusted RelayHubApi singleton to the constructor.
-// * Implement accept_relayed_call, which acts as a whitelist/blacklist of senders.  It is advised that the recipient's owner will be able to update that list to remove abusers.
-// * In every function that cares about the sender, use "address sender = get_sender()" instead of msg.sender.  It'll return msg.sender for non-relayed transactions, or the real sender in case of relayed transactions.
+// * Implement acceptRelayedCall, which acts as a whitelist/blacklist of senders.  It is advised that the recipient's owner will be able to update that list to remove abusers.
+// * In every function that cares about the sender, use "address sender = getSender()" instead of msg.sender.  It'll return msg.sender for non-relayed transactions, or the real sender in case of relayed transactions.
 
 import "./RelayRecipientApi.sol";
 import "./RelayHubApi.sol";
@@ -13,10 +13,10 @@ import "@0x/contracts-utils/contracts/src/LibBytes.sol";
 
 contract RelayRecipient is RelayRecipientApi {
 
-    RelayHubApi private relay_hub; // The RelayHubApi singleton which is allowed to call us
+    RelayHubApi private relayHub; // The RelayHubApi singleton which is allowed to call us
 
-	function get_hub_addr() public view returns (address) {
-		return address(relay_hub);
+	function getHubAddr() public view returns (address) {
+		return address(relayHub);
 	}
 
     /**
@@ -26,58 +26,68 @@ contract RelayRecipient is RelayRecipientApi {
      * This method might also be called by the owner, in order to use a new RelayHubApi - since the RelayHubApi
      * itself is not an upgradable contract.
      */
-    function init_relay_hub(RelayHubApi _rhub) internal {
-        require(relay_hub == RelayHubApi(0), "init_relay_hub: rhub already set");
-        set_relay_hub(_rhub);
+    function initRelayHub(RelayHubApi _rhub) internal {
+        require(relayHub == RelayHubApi(0), "initRelayHub: rhub already set");
+        setRelayHub(_rhub);
+    }
+    
+    function setRelayHub(RelayHubApi _rhub) internal {
+        // Normally called just once, during initRelayHub.
+        // Left as a separate internal function, in case a contract wishes to have its own update mechanism for RelayHub.
+        relayHub = _rhub;
+
+        //attempt a read method, just to validate the relay is a valid RelayHub contract.
+        getRecipientBalance();
     }
 
-    function set_relay_hub(RelayHubApi _rhub) internal {
-        // Normally called just once, during init_relay_hub.
-        // Left as a separate internal function, in case a contract wishes to have its own update mechanism for RelayHubApi.
-        relay_hub = _rhub;
-
-        //attempt a read method, just to validate the relay is a valid RelayHubApi contract.
-        get_recipient_balance();
-    }
-
-    function get_relay_hub() internal view returns (RelayHubApi) {
-        return relay_hub;
+    function getRelayHub() internal view returns (RelayHubApi) {
+        return relayHub;
     }
 
     /**
      * return the balance of this contract.
      * Note that this method will revert on configuration error (invalid relay address)
      */
-    function get_recipient_balance() public view returns (uint) {
-        return get_relay_hub().balanceOf(address(this));
+    function getRecipientBalance() public view returns (uint) {
+        return getRelayHub().balanceOf(address(this));
     }
 
-    function get_sender_from_data(address orig_sender, bytes memory msg_data) public view returns(address) {
-        address sender = orig_sender;
-        if (orig_sender == get_hub_addr() ) {
+    function getSenderFromData(address origSender, bytes memory msgData) public view returns(address) {
+        address sender = origSender;
+        if (origSender == getHubAddr() ) {
             // At this point we know that the sender is a trusted RelayHubApi, so we trust that the last bytes of msg.data are the verified sender address.
             // extract sender address from the end of msg.data
-            sender = LibBytes.readAddress(msg_data, msg_data.length - 20);
+            sender = LibBytes.readAddress(msgData, msgData.length - 20);
         }
         return sender;
     }
 
-    function get_sender() public view returns(address) {
-        return get_sender_from_data(msg.sender, msg.data);
+    function getSender() public view returns(address) {
+        return getSenderFromData(msg.sender, msg.data);
     }
 
-    function get_message_data() public view returns(bytes memory) {
-        bytes memory orig_msg_data = msg.data;
-        if (msg.sender == get_hub_addr()) {
+    function getMessageData() public view returns(bytes memory) {
+        bytes memory origMsgData = msg.data;
+        if (msg.sender == getHubAddr()) {
             // At this point we know that the sender is a trusted RelayHubApi, so we trust that the last bytes of msg.data are the verified sender address.
             // extract original message data from the start of msg.data
-            orig_msg_data = new bytes(msg.data.length - 20);
-            for (uint256 i = 0; i < orig_msg_data.length; i++)
+            origMsgData = new bytes(msg.data.length - 20);
+            for (uint256 i = 0; i < origMsgData.length; i++)
             {
-                orig_msg_data[i] = msg.data[i];
+                origMsgData[i] = msg.data[i];
             }
         }
-        return orig_msg_data;
+        return origMsgData;
     }
+    /*** Note :values below 10 are reserved by canRelay
+    *  @param encodedFunction the encoded function call (without any ethereum signature).
+    *  @param gasPrice - the gas price for this transaction
+    *  @param transactionFee - the relay compensation (in %) for this transaction
+    */
+    function acceptRelayedCall(address relay, address from, bytes memory encodedFunction, uint gasPrice, uint transactionFee, bytes memory approval) public view returns(uint32);
+    /** the method is given all parameters of acceptRelayedCall, and also the success/failure status and actual used gas.
+    * - usedGas - gas used up to this point. Note that gas calculation (for the purpose of compensation
+    */
+function postRelayedCall(address relay, address from, bytes memory encodedFunction, bool success, uint usedGas, uint transactionFee) public;
 }
 

contracts/SampleRecipient.sol
@@ -7,19 +7,19 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract SampleRecipient is RelayRecipient, Ownable {
 
-    mapping (address => bool) public relays_whitelist;
+    mapping (address => bool) public relaysWhitelist;
 
     constructor(RelayHubApi rhub) public {
-        init_relay_hub(rhub);
+        initRelayHub(rhub);
     }
 
     function deposit() public payable {
-        get_relay_hub().depositFor.value(msg.value)(address(this));
+        getRelayHub().depositFor.value(msg.value)(address(this));
     }
 
     function withdraw() public onlyOwner {
-        uint balance = get_relay_hub().balanceOf(address(this));
-        get_relay_hub().withdraw(balance);
+        uint balance = getRelayHub().balanceOf(address(this));
+        getRelayHub().withdraw(balance);
         msg.sender.transfer(balance);
     }
 
@@ -32,28 +32,28 @@ contract SampleRecipient is RelayRecipient, Ownable {
 
     function () external payable {}
 
-    event SampleRecipientEmitted(string message, address real_sender, address msg_sender, address origin);
+    event SampleRecipientEmitted(string message, address realSender, address msgSender, address origin);
     function emitMessage(string memory message) public {
-        emit SampleRecipientEmitted(message, get_sender(), msg.sender, tx.origin);
+        emit SampleRecipientEmitted(message, getSender(), msg.sender, tx.origin);
     }
 
-    function set_relay(address relay, bool on) public {
-        relays_whitelist[relay] = on;
+    function setRelay(address relay, bool on) public {
+        relaysWhitelist[relay] = on;
     }
 
     address public blacklisted;
 
-    function set_blacklisted(address addr) public {
+    function setBlacklisted(address addr) public {
         blacklisted = addr;
     }
 
-    function accept_relayed_call(address relay, address from, bytes memory /*encoded_function*/, uint /*gas_price*/, uint /*transaction_fee*/ , bytes memory approval) public view returns(uint32) {
+    function acceptRelayedCall(address relay, address from, bytes memory /*encodedFunction*/, uint /*gasPrice*/, uint /*transactionFee*/ , bytes memory approval) public view returns(uint32) {
         // The factory accepts relayed transactions from anyone, so we whitelist our own relays to prevent abuse.
         // This protection only makes sense for contracts accepting anonymous calls, and therefore not used by Gatekeeper or Multisig.
         // May be protected by a user_credits map managed by a captcha-protected web app or association with a google account.
-        if ( relays_whitelist[relay] ) return 0;
+        if ( relaysWhitelist[relay] ) return 0;
         if (from == blacklisted) return 11;
-
+        
         // this is an example of how the dapp can provide an offchain approval to a transaction
         if (approval.length == 65){
             // No owner signature given - proceed as usual (for existing tests)
@@ -69,11 +69,11 @@ contract SampleRecipient is RelayRecipient, Ownable {
 		return 0;
     }
 
-    event SampleRecipientPostCall(uint used_gas );
+    event SampleRecipientPostCall(uint usedGas );
 
-    function post_relayed_call(address /*relay*/ , address /*from*/, bytes memory /*encoded_function*/, bool /*success*/, uint used_gas, uint transaction_fee ) public {
+    function postRelayedCall(address /*relay*/ , address /*from*/, bytes memory /*encodedFunction*/, bool /*success*/, uint usedGas, uint transactionFee) public {
 
-        emit SampleRecipientPostCall(used_gas * tx.gasprice * (transaction_fee+100)/100);
+        emit SampleRecipientPostCall(usedGas * tx.gasprice * (transactionFee +100)/100);
     }
 
 }