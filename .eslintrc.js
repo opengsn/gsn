cfd4ecc722ec720c3fc8c7c77b93dc5b021a4948
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-07-01 10:00:47 -0300

    Fix balance check, add new tests (#137)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
+        "es6": true,
+        "jest": true,
+        "mocha": true,
         "node": true,
-        "es6": true
+    },
+    "globals" : {
+        "artifacts": false,
+        "assert": false,
+        "contract": false,
+        "web3": false,
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": 2017
     },
     "rules": {
-        "no-console": "off"
+        "no-console": "off",
     }
 };
