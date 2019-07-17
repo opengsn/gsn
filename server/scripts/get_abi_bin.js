3935592d7334de97c62ac5f92bf56ae99b46b7ab
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-07-17 20:14:45 -0300

    Make the Go code depend on IRelayHub

--- server/scripts/get_abi_bin.js
+++ server/scripts/get_abi_bin.js
@@ -2,9 +2,9 @@
 
 var fs = require("fs");
 
-var contractsToExtract =[ "RelayHub", "SampleRecipient" ];
+var contractsToExtract =[ "IRelayHub", "SampleRecipient" ];
 
-// var rhub = require("../../build/contracts/RelayHub.json");
+// var rhub = require("../../build/contracts/IRelayHub.json");
 // var sampleRec = require("../../build/contracts/SampleRecipient.json");
 
 contractsToExtract.forEach( name => {
@@ -14,7 +14,7 @@ contractsToExtract.forEach( name => {
     fs.writeFileSync("../build/server/contracts/"+name+".bin", contract.bytecode);
 });
 
-// fs.writeFileSync("../build/contracts/RelayHub.abi", JSON.stringify(rhub.abi));
-// fs.writeFileSync("../build/contracts/RelayHub.bin", JSON.stringify(rhub.bytecode));
+// fs.writeFileSync("../build/contracts/IRelayHub.abi", JSON.stringify(rhub.abi));
+// fs.writeFileSync("../build/contracts/IRelayHub.bin", JSON.stringify(rhub.bytecode));
 // fs.writeFileSync("../build/contracts/SampleRecipient.abi", JSON.stringify(sampleRec.abi));
 // fs.writeFileSync("../build/contracts/SampleRecipient.bin", JSON.stringify(sampleRec.bytecode));
