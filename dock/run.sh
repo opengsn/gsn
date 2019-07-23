91655be61654882ee01ee8122cdcb257aeb7d3bd
Dror Tirosh <dror@tabookey.com>
2019-07-23 17:38:26 +0300

    release 0.4.0-beta2
    client npm package
    docker image (referenced from npm package)

--- dock/run.sh
+++ dock/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -e
+#launch command inside docker image - so that we can run build commands in a consistent environment
+#   on mac, linux.
+# current working folder is exposed inside the docker image
+# only the "build" and "node_modules" are separate (since host and docker might have different binary formats)
+# for this reason, you must run "yarn" to install all packages again.
+# actual build and node_modules are saved under build/dock-build and build/dock-node_modules
+# by default, ports 8545, 8090 are forwarded into the docker image (so we can run a relay and ganache inside)
+#
+#environment variables:
+#NO_PORT - disable port forwarding into the docker image.
+#VERBOSE - show docker image re-creation (by default, it takes only 1-2 seconds, so we hide it, but after checking
+#           out new version it might take longer)
 
 DOCKNAME=relaydock
 
@@ -48,9 +60,11 @@ if [ -t 1 ]; then TTY="-ti" ; else TTY="-i"; fi
 
 # trap onexit EXIT
 
+test -z "$NO_PORTS" && DOCK_PORTS="-p 8090:8090 -p 8545:8545"
+
 docker run $DOCK_OPT -u $UID:$GID  \
 	--name $DOCKNAME \
-	-p 8090:8090 -p 8545:8545 \
+	$DOCK_PORTS \
 	$ENVVARS --rm $TTY -v $TMP_PASSWD:/etc/passwd \
 	$FOLDERS $FOLDER_MAPS  \
 	-w `pwd` $DOCKNAME $*
