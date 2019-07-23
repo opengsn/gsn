38d4a407436a135a159d2126d3c11759642e9ceb
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-07-23 16:18:50 -0300

    Update singleton deployment script

--- scripts/singleton/compute.sh
+++ scripts/singleton/compute.sh
@@ -3,23 +3,18 @@
 # Exit script as soon as a command fails.
 set -o errexit
 
-solidity_version="0.5.8"
-if solc --version | grep -q "$solidity_version" ; then
-  echo "Will compile using solc v$solidity_version"
-else
-  echo "solc version v$solidity_version is required"
-  exit 1
-fi
-
 output_dir="singleton"
 
-echo "Storing artifacts in directory '$output_dir'"
+echo "Will store artifacts in directory '$output_dir'"
 
 rm -rf "$output_dir" # Delete possible remains from previous run
 mkdir -p "$output_dir"
+
+echo "Flattening source files into a single file"
 npx truffle-flattener contracts/RelayHub.sol > $output_dir/RelayHub.flattened.sol
 
-solc --optimize --optimize-runs 200 --metadata-literal --combined-json abi,bin,metadata "$output_dir/RelayHub.flattened.sol" > "$output_dir/RelayHub.flattened.json"
+echo "Compiling with solcjs version $(npx solcjs --version)"
+npx solcjs --optimize --optimize-runs 200 --abi --bin --output-dir $output_dir "$output_dir/RelayHub.flattened.sol"
 
 node scripts/singleton/get-deploy-data.js > "$output_dir/deploy.json"
 echo "Stored deployment data in $output_dir/deploy.json"
