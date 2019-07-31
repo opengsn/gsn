9507231fd7dcbf36142d83a93e2e2a2a99c6bb7b
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-07-31 13:22:22 -0300

    Add singleton deployment documentation

--- scripts/singleton/deploy.js
+++ scripts/singleton/deploy.js
@@ -1,6 +1,8 @@
 const deployData = require('../../singleton/deploy.json');
 const { BN, toWei } = require('web3-utils');
 
+// Deploys a RelayHub instance, reading required data from singleton/deploy.json, the output of scripts/singleton/compute.sh
+// Call this file with truffle exec on the target network.
 async function deploy() {
   const funder = (await web3.eth.getAccounts())[0];
   await web3.eth.sendTransaction({ from: funder, to: deployData.deployer, value: new BN(toWei('0.42', 'ether')) });
