ec78db30086ed003896133ce6db9372d80e08860
Dror Tirosh <dror@openeth.dev>
2020-02-27 16:40:45 +0200

    fixed deployment script
    support new constructor parameter
    (and bypass "flattener" unsupported "pragma experimental")

--- scripts/singleton/deploy.js
+++ scripts/singleton/deploy.js
@@ -1,11 +1,10 @@
 const deployData = require('../../singleton/deploy.json')
-const { BN, toWei } = require('web3-utils')
 
 // Deploys a RelayHub instance, reading required data from singleton/deploy.json, the output of scripts/singleton/compute.sh
 // Call this file with truffle exec on the target network.
 async function deploy () {
   const funder = (await web3.eth.getAccounts())[0]
-  await web3.eth.sendTransaction({ from: funder, to: deployData.deployer, value: new BN(toWei('0.42', 'ether')) })
+  await web3.eth.sendTransaction({ from: funder, to: deployData.deployer, value: deployData.gas })
 
   await web3.eth.sendSignedTransaction(deployData.contract.deployTx)
   console.log(`Deployed at ${deployData.contract.address}`)
