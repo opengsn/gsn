7e08c850cb06800b5fe614ab768ce0d79f67f335
Alex Forshtat <forshtat1@gmail.com>
2020-04-01 19:39:53 +0300

    Remove unused docker files

--- prodserverdock/relay/getcert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-if ! grep -q CERTIFICATE data/$HOSTNAME.pem 2>/dev/null ; then
-
-  echo a|certbot certonly --register-unsafely-without-email --standalone --preferred-challenges http -d $HOSTNAME
-  cat /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /etc/letsencrypt/live/$HOSTNAME/privkey.pem > data/$HOSTNAME.pem
-
-fi
-
--- serverdock/start-relay-with-ganache.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-ganache-cli -l 8000000 -h 0.0.0.0 > geth.log 2>&1 &
-./start-relay.sh internal
