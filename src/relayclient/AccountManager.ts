65b70d129aabb29f39567dc692585fada5e7c032
Alex Forshtat <forshtat1@gmail.com>
2020-04-15 04:18:36 +0300

    Implement Account Manager tests

--- src/relayclient/AccountManager.ts
+++ src/relayclient/AccountManager.ts
@@ -15,6 +15,11 @@ export interface AccountKeypair {
   address: Address
 }
 
+function toAddress (wallet: any): string {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  return `0x${wallet.getAddress().toString('hex')}`
+}
+
 export default class AccountManager {
   private readonly web3: Web3
   private readonly accounts: AccountKeypair[] = []
@@ -28,6 +33,10 @@ export default class AccountManager {
   }
 
   addAccount (keypair: AccountKeypair): void {
+    const wallet = ethWallet.fromPrivateKey(keypair.privateKey)
+    if (!isSameAddress(toAddress(wallet), keypair.address)) {
+      throw new Error('invalid keypair')
+    }
     this.accounts.push(keypair)
   }
 
@@ -36,7 +45,7 @@ export default class AccountManager {
     const keypair = {
       privateKey: a.privKey,
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      address: `0x${a.getAddress().toString('hex')}`
+      address: toAddress(a)
     }
     this.addAccount(keypair)
     return keypair
@@ -45,34 +54,52 @@ export default class AccountManager {
   // TODO: make forwarder part of RelayRequest, why is it dangling??
   async sign (relayRequest: RelayRequest, forwarderAddress: Address): Promise<PrefixedHexString> {
     let signature
-    const signedData = await getDataToSign({
+    const signedData = getDataToSign({
       chainId: this.chainId,
       verifier: forwarderAddress,
       relayRequest
     })
     const keypair = this.accounts.find(account => isSameAddress(account.address, relayRequest.relayData.senderAddress))
     if (keypair != null) {
-      // @ts-ignore
-      signature = sigUtil.signTypedData_v4(this.ephemeralKeypair.privateKey, { data: signedData })
+      signature = this._signWithControlledKey(keypair, signedData)
     } else {
-      signature = await getEip712Signature(
-        {
-          web3: this.web3,
-          methodSuffix: this.config.methodSuffix ?? '',
-          jsonStringifyRequest: this.config.jsonStringifyRequest ?? false,
-          dataToSign: signedData
-        })
+      signature = await this._signWithExternallyControlledKey(signedData)
     }
-    // Sanity check only
-    // @ts-ignore
-    const rec = sigUtil.recoverTypedSignature_v4({
-      data: signedData,
-      sig: signature
-    })
+
+    // Sanity check
+    let rec: Address
+    try {
+      // @ts-ignore
+      rec = sigUtil.recoverTypedSignature_v4({
+        data: signedData,
+        sig: signature
+      })
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      throw new Error(`Failed to sign relayed transaction for ${relayRequest.relayData.senderAddress}`)
+    }
     if (!isSameAddress(relayRequest.relayData.senderAddress.toLowerCase(), rec)) {
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      throw new Error(`signature error: sender=${relayRequest.relayData.senderAddress}, recovered=${rec}`)
+      throw new Error(`Internal RelayClient exception: signature is not correct: sender=${relayRequest.relayData.senderAddress}, recovered=${rec}`)
     }
     return signature
+  }
+
+  // These methods is extracted to
+  // a) allow different implementations in the future, and
+  // b) allow spying on Account Manager in tests
+  async _signWithExternallyControlledKey (signedData: any): Promise<string> {
+    return getEip712Signature(
+      {
+        web3: this.web3,
+        methodSuffix: this.config.methodSuffix ?? '',
+        jsonStringifyRequest: this.config.jsonStringifyRequest ?? false,
+        dataToSign: signedData
+      })
+  }
+
+  _signWithControlledKey (keypair: AccountKeypair, signedData: any): string {
+    // @ts-ignore
+    return sigUtil.signTypedData_v4(keypair.privateKey, { data: signedData })
   }
 }
---
+++ test/relayclient/AccountManager.test.ts
@@ -0,0 +1,116 @@
+import AccountManager from '../../src/relayclient/AccountManager'
+import { defaultEnvironment } from '../../src/relayclient/types/Environments'
+import { HttpProvider } from 'web3-core'
+import RelayRequest from '../../src/common/EIP712/RelayRequest'
+import { constants } from '@openzeppelin/test-helpers'
+import sinon from 'sinon'
+import sigUtil from 'eth-sig-util'
+import getDataToSign from '../../src/common/EIP712/Eip712Helper'
+import { isSameAddress } from '../../src/common/utils'
+import chai from 'chai'
+import sinonChai from 'sinon-chai'
+
+const expect = chai.expect
+chai.use(sinonChai)
+
+contract('AccountManager', function (accounts) {
+  const address = '0x982a8CbE734cb8c29A6a7E02a3B0e4512148F6F9'
+  const keypair = {
+    privateKey: Buffer.from('d353907ab062133759f149a3afcb951f0f746a65a60f351ba05a3ebf26b67f5c', 'hex'),
+    address
+  }
+  const config = {
+    verbose: false,
+    methodSuffix: '',
+    jsonStringifyRequest: false
+  }
+  const accountManager = new AccountManager(web3.currentProvider as HttpProvider, defaultEnvironment.chainId, config)
+  // @ts-ignore
+  sinon.spy(accountManager)
+  describe('#addAccount()', function () {
+    it('should save the provided keypair internally', function () {
+      accountManager.addAccount(keypair)
+      // @ts-ignore
+      assert.equal(accountManager.accounts[0].privateKey.toString(), keypair.privateKey.toString())
+      // @ts-ignore
+      assert.equal(accountManager.accounts[0].address, keypair.address)
+    })
+
+    it('should throw if the provided keypair is not valid', function () {
+      const keypair = {
+        privateKey: Buffer.from('AAAAAAAAAAAAA6a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d', 'hex'),
+        address
+      }
+      expect(() => {
+        accountManager.addAccount(keypair)
+      }).to.throw('invalid keypair')
+    })
+  })
+  describe('#newAccount()', function () {
+    const accountManager = new AccountManager(web3.currentProvider as HttpProvider, defaultEnvironment.chainId, config)
+    it('should create a new keypair, return it and save it internally', function () {
+      const keypair = accountManager.newAccount()
+      // @ts-ignore
+      assert.equal(accountManager.accounts[0].privateKey.toString(), keypair.privateKey.toString())
+    })
+  })
+
+  describe('#sign()', function () {
+    accountManager.addAccount(keypair)
+    const relayRequest = new RelayRequest({
+      senderAddress: '',
+      encodedFunction: '0x123',
+      senderNonce: '1',
+      target: constants.ZERO_ADDRESS,
+      pctRelayFee: '1',
+      baseRelayFee: '1',
+      gasPrice: '1',
+      gasLimit: '1',
+      relayWorker: constants.ZERO_ADDRESS,
+      paymaster: constants.ZERO_ADDRESS
+    })
+    beforeEach(function () {
+      sinon.resetHistory()
+    })
+
+    it('should use internally controlled keypair for signing if available', async function () {
+      relayRequest.relayData.senderAddress = address
+      const signedData = getDataToSign({
+        chainId: defaultEnvironment.chainId,
+        verifier: constants.ZERO_ADDRESS,
+        relayRequest
+      })
+      const signature = await accountManager.sign(relayRequest, constants.ZERO_ADDRESS)
+      // @ts-ignore
+      const rec = sigUtil.recoverTypedSignature_v4({
+        data: signedData,
+        sig: signature
+      })
+      assert.ok(isSameAddress(relayRequest.relayData.senderAddress.toLowerCase(), rec))
+      expect(accountManager._signWithControlledKey).to.have.been.calledWith(keypair, signedData)
+      expect(accountManager._signWithExternallyControlledKey).to.have.not.been.called
+    })
+    it('should ask web3 to sign if key is not controlled', async function () {
+      relayRequest.relayData.senderAddress = accounts[0]
+      const signedData = getDataToSign({
+        chainId: defaultEnvironment.chainId,
+        verifier: constants.ZERO_ADDRESS,
+        relayRequest
+      })
+      const signature = await accountManager.sign(relayRequest, constants.ZERO_ADDRESS)
+      // @ts-ignore
+      const rec = sigUtil.recoverTypedSignature_v4({
+        data: signedData,
+        sig: signature
+      })
+      assert.ok(isSameAddress(relayRequest.relayData.senderAddress.toLowerCase(), rec))
+      expect(accountManager._signWithExternallyControlledKey).to.have.been.calledWith(signedData)
+      expect(accountManager._signWithControlledKey).to.have.not.been.called
+    })
+    it('should throw if web3 fails to sign with requested address', function () {
+      relayRequest.relayData.senderAddress = '0x4cfb3f70bf6a80397c2e634e5bdd85bc0bb189ee'
+      const promise = accountManager.sign(relayRequest, constants.ZERO_ADDRESS)
+      return expect(promise).to.be.eventually.rejectedWith('Failed to sign relayed transaction for 0x4cfb3f70bf6a80397c2e634e5bdd85bc0bb189ee')
+    })
+  })
+})
