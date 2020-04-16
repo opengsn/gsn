35d43dc8414103b6365c64e6f2bbf081a82de574
Dror Tirosh <dror@openeth.dev>
2020-04-16 19:38:13 +0300

    lints

--- src/relayclient/GsnDevProvider.ts
+++ src/relayclient/GsnDevProvider.ts
@@ -1,23 +1,21 @@
 import RelayProvider from './RelayProvider'
-import {DevRelayClient, DevClientConfig} from './DevRelayClient'
+import { DevRelayClient, DevClientConfig } from './DevRelayClient'
 import RelayClient from './RelayClient'
 import { HttpProvider } from 'web3-core'
 import { GSNConfig } from './GSNConfigurator'
 
 export default class GsnDevProvider extends RelayProvider {
-
-  devRelayClient : DevRelayClient
+  devRelayClient: DevRelayClient
 
   /**
    * Create a dev provider.
    * Create a provider that brings up an in-process relay.
    */
   constructor (origProvider: HttpProvider, gsnConfig: GSNConfig, devConfig: DevClientConfig, devRelayClient?: DevRelayClient) {
-
     const client = devRelayClient ?? new DevRelayClient(
-        RelayClient.getDefaultDependencies(origProvider, gsnConfig),
-        gsnConfig.relayHubAddress,
-        gsnConfig.relayClientConfig, devConfig)
+      RelayClient.getDefaultDependencies(origProvider, gsnConfig),
+      gsnConfig.relayHubAddress,
+      gsnConfig.relayClientConfig, devConfig)
     super(origProvider, gsnConfig, client)
     this.devRelayClient = client
   }
