e5faf299edd4e7892255964403a94a819c8dd520
shahafn <shahaflol@gmail.com>
2020-04-17 05:00:33 +0300

    Fixing bug

--- test/RelayHubPenalizations.test.ts
+++ test/RelayHubPenalizations.test.ts
@@ -25,8 +25,8 @@ const StakeManager = artifacts.require('StakeManager')
 const Penalizer = artifacts.require('Penalizer')
 const TestRecipient = artifacts.require('TestRecipient')
 const TestPaymasterEverythingAccepted = artifacts.require('TestPaymasterEverythingAccepted')
-var fix = false
-contract.only('RelayHub Penalizations', function ([_, relayOwner, relayWorker, otherRelayWorker, sender, other, relayManager, otherRelayManager, thirdRelayWorker]) { // eslint-disable-line no-unused-vars
+
+contract('RelayHub Penalizations', function ([_, relayOwner, relayWorker, otherRelayWorker, sender, other, relayManager, otherRelayManager, thirdRelayWorker]) { // eslint-disable-line no-unused-vars
   const chainId = defaultEnvironment.chainId
 
   let stakeManager: StakeManagerInstance
@@ -98,7 +98,7 @@ contract.only('RelayHub Penalizations', function ([_, relayOwner, relayWorker, o
       return receipt
     }
 
-    describe.only('penalizable behaviors', function () {
+    describe('penalizable behaviors', function () {
       const encodedCallArgs = {
         sender,
         recipient: '0x1820b744B33945482C17Dc37218C01D858EBc714',
@@ -151,38 +151,9 @@ contract.only('RelayHub Penalizations', function ([_, relayOwner, relayWorker, o
           )
         })
 
-        it.only('penalizes transactions with same nonce and different value', async function () {
-          let newArgs
-          let txDataSigA: any
-          let txDataSigB: any
-          let i = 0
-          do {
-            newArgs = {...encodedCallArgs, gasLimit: encodedCallArgs.gasLimit + i++}
-            txDataSigA = getDataAndSignature(encodeRelayCallEIP155(newArgs, relayCallArgs), chainId)
-            txDataSigB = getDataAndSignature(encodeRelayCallEIP155(newArgs, Object.assign({}, relayCallArgs, { value: 100 })), chainId)
-          } while (txDataSigA.signature.length === txDataSigB.signature.length)
-          // const txDataSigA = getDataAndSignature(encodeRelayCallEIP155(encodedCallArgs, relayCallArgs), chainId)
-          // const txDataSigB = getDataAndSignature(encodeRelayCallEIP155(encodedCallArgs, Object.assign({}, relayCallArgs, { value: 100 })), chainId)
-
-          try {
-            await expectPenalization(async (opts) =>
-              penalizer.penalizeRepeatedNonce(txDataSigA.data, txDataSigA.signature, txDataSigB.data, txDataSigB.signature, relayHub.address, opts)
-            )
-          } catch (e) {
-            if (e.message.includes('Different signer')) {
-              console.log('TADA!')
-              console.log('encodedCallArgs', encodedCallArgs)
-              console.log('relayCallArgs', relayCallArgs)
-              console.log('txDataSigA', txDataSigA)
-              console.log('txDataSigB', txDataSigB)
-            } else {
-              assert.fail(e.message)
-            }
-          }
-          console.log('now FIX!')
-          fix = true
-          txDataSigA = getDataAndSignature(encodeRelayCallEIP155(newArgs, relayCallArgs), chainId)
-          txDataSigB = getDataAndSignature(encodeRelayCallEIP155(newArgs, Object.assign({}, relayCallArgs, { value: 100 })), chainId)
+        it('penalizes transactions with same nonce and different value', async function () {
+          const txDataSigA = getDataAndSignature(encodeRelayCallEIP155(encodedCallArgs, relayCallArgs), chainId)
+          const txDataSigB = getDataAndSignature(encodeRelayCallEIP155(encodedCallArgs, Object.assign({}, relayCallArgs, { value: 100 })), chainId)
 
           await expectPenalization(async (opts) =>
             penalizer.penalizeRepeatedNonce(txDataSigA.data, txDataSigA.signature, txDataSigB.data, txDataSigB.signature, relayHub.address, opts)
@@ -403,8 +374,9 @@ contract.only('RelayHub Penalizations', function ([_, relayOwner, relayWorker, o
       })
 
       transaction.sign(Buffer.from(relayCallArgs.privateKey, 'hex'))
-      transaction.r = Buffer.concat([Buffer.from('00'.repeat(32 - transaction.r.length),'hex'), transaction.r])
-      transaction.s = Buffer.concat([Buffer.from('00'.repeat(32 - transaction.s.length),'hex'), transaction.s])
+      // This doesn't work since they are read only for some reason
+      // transaction.r = Buffer.concat([Buffer.from('00'.repeat(32 - transaction.r.length),'hex'), transaction.r])
+      // transaction.s = Buffer.concat([Buffer.from('00'.repeat(32 - transaction.s.length),'hex'), transaction.s])
 
       return transaction
     }
@@ -457,32 +429,8 @@ contract.only('RelayHub Penalizations', function ([_, relayOwner, relayWorker, o
         v -= chainId * 2 + 8
       }
       const data = `0x${encode(input).toString('hex')}`
-      let fixed = false
-      if (fix && tx.r.length !== 32) {
-        console.log('XXXXXXXXXXXXXXXXXXXXXXXXXX should fail here! r', tx.r.length, tx.r)
-        console.log('wtf zero buf', Buffer.from('00'.repeat(32 - tx.r.length),'hex'))
-        tx.r = Buffer.concat([Buffer.from('00'.repeat(32 - tx.r.length),'hex'), tx.r])
-        console.log('after length', tx.r.length, tx.r)
-        console.log('fuck me', Buffer.concat([Buffer.from('00'.repeat(32 - tx.r.length),'hex'), tx.r]))
-        console.log('again', tx.r)
-        fixed = true
-      }
-      if (fix && tx.s.length !== 32) {
-        console.log('XXXXXXXXXXXXXXXXXXXXXXXXXX should fail here! s', tx.s.length, tx.s)
-        console.log('wtf zero buf', Buffer.from('00'.repeat(32 - tx.s.length),'hex'))
-        tx.s = Buffer.concat([Buffer.from('00'.repeat(32 - tx.s.length),'hex'), tx.s])
-        console.log('after length', tx.s.length, tx.s)
-        console.log('fuck me', Buffer.concat([Buffer.from('00'.repeat(32 - tx.s.length),'hex'), tx.s]))
-        console.log('again', tx.s)
-        fixed = true
-      }
-      let signature = `0x${tx.r.toString('hex')}${tx.s.toString('hex')}${v.toString(16)}`
-      if (fixed) {
-        console.log('before sig', signature)
-        console.log('tx serialized', tx.serialize().toString('hex'))
-        signature = `0x${'00'.repeat(32 - tx.r.length) + tx.r.toString('hex')}${'00'.repeat(32 - tx.s.length) + tx.s.toString('hex')}${v.toString(16)}`
-        console.log('signature fixed', signature.length, signature)
-      }
+      const signature = `0x${'00'.repeat(32 - tx.r.length) + tx.r.toString('hex')}${'00'.repeat(
+        32 - tx.s.length) + tx.s.toString('hex')}${v.toString(16)}`
       return {
         data,
         signature
