e4c235c14a083d9f672e222ce00f90e1075f6429
shahafn <shahaflol@gmail.com>
2020-04-23 16:49:51 +0300

    Fixes
    - Calling setHub() after deploying paymaster in gsn-helper
    - Using transaction.getSenderAddress() which verifies signature while
    taking into account chainId for recovery

--- helpers/src/deploy.js
+++ helpers/src/deploy.js
@@ -1,5 +1,6 @@
 const data = require('./data')
-const { defaultFromAccount, saveContractToFile, getRelayHub, getPenalizer, getStakeManager, getPaymaster } = require('./helpers')
+const { defaultFromAccount, saveContractToFile, getRelayHub, getPenalizer, getStakeManager, getPaymaster } = require(
+  './helpers')
 const { merge } = require('lodash')
 
 async function deployRelayHub (web3, options = {}) {
@@ -13,36 +14,44 @@ async function deployRelayHub (web3, options = {}) {
   const stakeManager = getStakeManager(web3)
   const sInstance = await stakeManager.deploy({
     data: data.stakeManager.bytecode
-  }).send({from: options.from,
-    gas:1e8,
-    gasPrice: 1e9,
+  }).send({
+    from: options.from,
+    gas: 1e8,
+    gasPrice: 1e9
   })
   saveContractToFile(sInstance, options.workdir, 'StakeManager.json')
   const penalizer = getPenalizer(web3)
   const pInstance = await penalizer.deploy({
     data: data.penalizer.bytecode
-  }).send({from: options.from,
-    gas:1e8,
-    gasPrice: 1e9,
+  }).send({
+    from: options.from,
+    gas: 1e8,
+    gasPrice: 1e9
   })
   saveContractToFile(pInstance, options.workdir, 'Penalizer.json')
   const relayHub = getRelayHub(web3)
   console.log('stakeManager ', sInstance.options.address)
   console.log('penalizer ', pInstance.options.address)
   const rInstance = await relayHub.deploy({
-    arguments: [16, sInstance.options.address, pInstance.options.address],
-  }).send({from: options.from,
-    gas:1e8,
-    gasPrice: 1e9,
+    arguments: [16, sInstance.options.address, pInstance.options.address]
+  }).send({
+    from: options.from,
+    gas: 1e8,
+    gasPrice: 1e9
   })
   saveContractToFile(rInstance, options.workdir, 'RelayHub.json')
   const paymaster = getPaymaster(web3)
-  const pmInstance = await paymaster.deploy({
-  }).send({from: options.from,
-    gas:1e8,
-    gasPrice: 1e9,
+  const pmInstance = await paymaster.deploy({}).send({
+    from: options.from,
+    gas: 1e8,
+    gasPrice: 1e9
   })
   saveContractToFile(pmInstance, options.workdir, 'Paymaster.json')
+  await pmInstance.methods.setHub(rInstance.options.address).send({
+    from: options.from,
+    gas: 1e8,
+    gasPrice: 1e9
+  })
   console.log('paymaster ', pmInstance.options.address)
   if (options.verbose) console.error(`RelayHub deployed at ${rInstance.options.address}`)
 
