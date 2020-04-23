5caef91742516e84fdcee78df307962d23abb729
Dror Tirosh <dror@opengsn.org>
2020-04-24 00:33:17 +0300

    Fix payable with getSender()
    getSender() does SLOAD (to get the Forwarder), and thus (after istanbul
    fork) is much more expensive than msg.sender.
    If used from the default payable function, it might cause the payable to
    revert.
    
    The fix: make sure not to read storage in case the msg.data is shorter,
    thus getSender() becomes much less expensive.

---
+++ test/regressions/PayableWithEmit.test.ts
@@ -0,0 +1,17 @@
+// import { getLogs } from '../TestUtils'
+const PayableWithEmit = artifacts.require('./PayableWithEmit.sol')
+
+contract('PayableWithEmit', () => {
+  let sender: any
+  let receiver: any
+
+  before(async () => {
+    receiver = await PayableWithEmit.new()
+    sender = await PayableWithEmit.new()
+  })
+  it('payable that uses getSender', async () => {
+    const ret = await sender.doSend(receiver.address, { value: 1e18 })
+    // console.log({ gasUsed: ret.receipt.gasUsed, log: getLogs(ret) })
+    assert.equal(ret.logs.find((e: any) => e.event === 'GasUsed').args.success, true)
+  })
+})
