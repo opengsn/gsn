f198de726a3591af7656437281ce8e5a6c5529b4
Dror Tirosh <dror@opengsn.org>
2020-04-29 17:08:59 +0300

    make _msgSender() return "address payable"

--- contracts/BaseRelayRecipient.sol
+++ contracts/BaseRelayRecipient.sol
@@ -32,7 +32,7 @@ contract BaseRelayRecipient is IRelayRecipient {
      * otherwise, return `msg.sender`.
      * should be used in the contract anywhere instead of msg.sender
      */
-    function _msgSender() internal view returns (address) {
+    function _msgSender() internal view returns (address payable) {
         if (msg.data.length >= 24 && msg.sender == address(getTrustedForwarder())) {
             // At this point we know that the sender is a trusted forwarder,
             // so we trust that the last bytes of msg.data are the verified sender address.
