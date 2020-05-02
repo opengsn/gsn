034ffc3110beb763f2231fdb84cda615f9dcddd0
Dror Tirosh <dror@opengsn.org>
2020-05-02 18:47:30 +0300

    remove commit.

--- index.ts
+++ index.ts
@@ -1,5 +1,2 @@
 export * from './RelayProvider'
 export * from './RelayClient'
-
-//not included: clients should ask for it explicitly (require '@opengsn/gsn/dist/GsnDevProvider')
-//export * from './GsDevProvider'
