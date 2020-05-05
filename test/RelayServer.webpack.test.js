d08c9f7959b34bc737764e0934b978d7462ca8fd
Dror Tirosh <dror@opengsn.org>
2020-05-05 20:09:30 +0300

    PR fixes

--- test/RelayServer.webpack.test.js
+++ test/RelayServer.webpack.test.js
@@ -1,9 +1,8 @@
-// eslint-disable-next-line no-unused-vars
 import childProcess from 'child_process'
 import path from 'path'
 import fs from 'fs'
 
-describe.only('RelayServer-webpack', () => {
+describe('RelayServer-webpack', () => {
   let oneFileRelayer
   before('create webpack', function () {
     this.timeout(5000)
@@ -13,14 +12,13 @@ describe.only('RelayServer-webpack', () => {
     oneFileRelayer = path.join(jsrelayDir, 'dist', 'relayserver.js')
   })
 
-  it('should launch (and say "missing address")', () => {
+  it('should launch (and say "missing address") to veirfy it was packed ok', () => {
     try {
       childProcess.execSync('node ' + oneFileRelayer, { encoding: 'ascii' })
       assert.fail('should throw')
     } catch (e) {
       assert.match(e.toString(), /missing --RelayHubAddress/)
     }
-    // just verify it launches
   })
 
   it('should test it can actually work')
