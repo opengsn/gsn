be65ebd38f3efcf2176a71ed0f4f3dfd0693dbbe
Alex Forshtat <forshtat1@gmail.com>
2020-05-31 03:10:56 +0300

    KeyManager

--- test/KeyManager.test.ts
+++ test/KeyManager.test.ts
@@ -1,14 +1,15 @@
 /* global */
 
-const fs = require('fs')
-const { KeyManager } = require('../src/relayserver/KeyManager')
+import fs from 'fs'
+import { KeyManager } from '../src/relayserver/KeyManager'
+
 const KEYSTORE_FILENAME = 'keystore'
 
 // NOTICE: this dir is removed in 'after', do not use this in any other test
 const workdir = '/tmp/gsn/test/key_manager'
 const keyStoreFilePath = workdir + '/' + KEYSTORE_FILENAME
 
-function cleanFolder () {
+function cleanFolder (): void {
   if (fs.existsSync(keyStoreFilePath)) {
     fs.unlinkSync(keyStoreFilePath)
   }
@@ -17,40 +18,42 @@ function cleanFolder () {
   }
 }
 
-if (!contract.only) { contract.only = contract } // buidler "support"
-
 contract('KeyManager', function (accounts) {
   describe('in-memory', () => {
-    let mkm
+    let mkm: KeyManager
+
     before(() => {
-      mkm = new KeyManager(10, null, 'seed1234')
+      mkm = new KeyManager(10, undefined, 'seed1234')
     })
     it('should return key', () => {
       // for a given seed, the addresses and privkeys are known..
       const k0 = mkm.getAddress(0)
+      // @ts-ignore
       assert.deepEqual(mkm._privateKeys[k0].toString('hex'),
         '98bd175008b68dfd5a6aca0584d5a040032f2469656569d5d428161b776d27ff')
       assert.equal(k0, '0x56558253d657baa29cfe9f0a808b7d19d5d80b9c')
     })
     it('should return another key for different index', () => {
       const k1 = mkm.getAddress(1)
+      // @ts-ignore
       assert.equal(mkm._privateKeys[k1].toString('hex'),
         'e52f32d373b0b38be3800ec9070af883a63c4fd2857c5b0f249180a2c303eb7e')
       assert.equal(k1, '0xe2ceef58b3e5a8816c52b00067830b8e1afd82da')
     })
   })
   describe('file-based KeyManager', () => {
-    let fkmA
+    let fkmA: KeyManager
 
-    before('create key manager', async function () {
+    before('create key manager', function () {
       cleanFolder()
       fkmA = new KeyManager(20, workdir)
       assert.isTrue(fs.existsSync(workdir), 'test keystore dir should exist already')
     })
 
-    it('should get key pair', async function () {
+    it('should get key pair', function () {
       const key = fkmA.getAddress(1)
       assert.isTrue(web3.utils.isAddress(key))
+      // @ts-ignore
       assert.equal(fkmA._privateKeys[key].length, 32)
     })
 
@@ -60,10 +63,12 @@ contract('KeyManager', function (accounts) {
       const fkmB = new KeyManager(20, workdir)
       const addrB = fkmB.getAddress(0)
       assert.equal(addrA, addrB)
+      // @ts-ignore
       assert.equal(fkmA._privateKeys[addrA].toString('hex'), fkmB._privateKeys[addrB].toString('hex'))
 
       const addrB10 = fkmB.getAddress(10)
       assert.equal(addrA10, addrB10)
+      // @ts-ignore
       assert.equal(fkmA._privateKeys[addrA10].toString('hex'), fkmB._privateKeys[addrB10].toString('hex'))
     })
 
