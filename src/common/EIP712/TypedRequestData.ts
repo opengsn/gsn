c12e81cd102f24d80a1469d6aba8ebb2c9bc002b
Alex Forshtat <forshtat1@gmail.com>
2020-06-22 01:20:58 +0300

    Fix parameter order

--- src/common/EIP712/TypedRequestData.ts
+++ src/common/EIP712/TypedRequestData.ts
@@ -21,12 +21,12 @@ const RelayDataType = [
 ]
 
 const ForwardRequestType = [
-  { name: 'from', type: 'address' },
   { name: 'to', type: 'address' },
   { name: 'data', type: 'bytes' },
   { name: 'value', type: 'uint256' },
-  { name: 'gas', type: 'uint256' },
-  { name: 'nonce', type: 'uint256' }
+  { name: 'from', type: 'address' },
+  { name: 'nonce', type: 'uint256' },
+  { name: 'gas', type: 'uint256' }
 ]
 
 const RelayRequestType = [
