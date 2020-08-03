ec009ec0339b5aef75dd54614e2acd5d55c7db0f
Dror Tirosh <dror@opengsn.org>
2020-08-03 22:33:56 +0300

    fix to make dockers work on server
    the launch command is:
    
    ```
    alias relaydc='docker run --rm  -e ROOT=$PWD -v
    /var/run/docker.sock:/var/run docker.sock -v $PWD/.env:/.env
    opengsn/relaydc'
    ```

--- dockers/prefixrouter/dbuild.sh
+++ dockers/prefixrouter/dbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 IMAGE=opengsn/prefixrouter
 docker build -t $IMAGE `dirname $0`
-VERSION=`jq < ../../package.json -r .version`
+test -z "$VERSION" && VERSION=`jq < ../../package.json -r .version`
 
 docker tag $IMAGE $IMAGE:$VERSION
 echo "== To publish"
-echo "   docker push $IMAGE; docker push $IMAGE:$VERSION"
+echo "   docker push $IMAGE:latest; docker push $IMAGE:$VERSION"
 
--- dockers/relaydc/dbuild.sh
+++ dockers/relaydc/dbuild.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 IMAGE=opengsn/relaydc
 docker build -t $IMAGE `dirname $0`
-VERSION=`jq < ../../package.json -r .version`
-
+test -z "$VERSION" && VERSION=`jq < ../../package.json -r .version`
 docker tag $IMAGE $IMAGE:$VERSION
 echo "== To publish"
-echo "   docker push $IMAGE; docker push $IMAGE:$VERSION"
+echo "   docker push $IMAGE:latest; docker push $IMAGE:$VERSION"
 
