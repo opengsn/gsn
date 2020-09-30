535d57640a6f7cd343602e1e3e94a16ca0614f4e
Dror Tirosh <dror@opengsn.org>
2020-09-30 14:53:20 +0300

    fix buggy registerForwarderForGsn
    moral: do not modify parameters, ever
    (sendOptions is used later by other contract creation, and break...)

--- src/common/EIP712/ForwarderUtil.ts
+++ src/common/EIP712/ForwarderUtil.ts
@@ -4,13 +4,15 @@ import { Contract } from 'web3-eth-contract'
 
 // register a forwarder for use with GSN: the request-type and domain separator we're using.
 export async function registerForwarderForGsn (forwarderTruffleOrWeb3: IForwarderInstance|Contract, sendOptions: any = undefined): Promise<void> {
+  let options = sendOptions
   let forwarder: Contract
   if ((forwarderTruffleOrWeb3 as any).contract != null) {
     forwarder = (forwarderTruffleOrWeb3 as any).contract
     // truffle-contract carries default options (e.g. from) in the object.
     // @ts-ignore
-    sendOptions = { ...forwarderTruffleOrWeb3.constructor.defaults(), ...sendOptions }
+    options = { ...forwarderTruffleOrWeb3.constructor.defaults(), ...sendOptions }
   } else {
+    options = sendOptions
     forwarder = forwarderTruffleOrWeb3 as any
   }
 
@@ -26,7 +28,7 @@ export async function registerForwarderForGsn (forwarderTruffleOrWeb3: IForwarde
   await logTx(forwarder.methods.registerRequestType(
     GsnRequestType.typeName,
     GsnRequestType.typeSuffix
-  ).send(sendOptions))
+  ).send(options))
 
-  await logTx(forwarder.methods.registerDomainSeparator(GsnDomainSeparatorType.name, GsnDomainSeparatorType.version).send(sendOptions))
+  await logTx(forwarder.methods.registerDomainSeparator(GsnDomainSeparatorType.name, GsnDomainSeparatorType.version).send(options))
 }
