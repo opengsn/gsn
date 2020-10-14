01b712ef0fa7be8b5e50c627733f8d61bfe5bd27
Dror Tirosh <dror@opengsn.org>
2020-10-14 22:06:01 +0300

    OG-280 relayclient initialization outside of first transaction (#516)
    * OG-280 relayclient initialization outside of first transaction
    
    default construction should be: await new RelayClient(...).init()
    Can still construct the old way, but it will generate a warning (since
    it delays the first transaction)
    
    NOTE: init() is now guarded with mutex, since its possible that
    relayTransaction() will be called before original init() finished.

--- src/relayclient/GsnEvents.ts
+++ src/relayclient/GsnEvents.ts
@@ -2,44 +2,45 @@
  * base export class for all events fired by RelayClient.
  * for "progress" report, it is enough to test the base export class only.
  * subclasses contain some extra information about the events.
+ * Last event is when we receive response from relayer that event was sent - now we should wait for mining..
  */
-const TOTAL_EVENTS = 8
+const TOTAL_EVENTS = 7
 export class GsnEvent {
   total = TOTAL_EVENTS
   constructor (readonly event: string, readonly step: number) {}
 }
 
-// initialize client (takes time only on first request)
+// initialize client (should be done before all requests. not counted in "total")
 export class GsnInitEvent extends GsnEvent {
-  constructor () { super('init', 1) }
+  constructor () { super('init', 0) }
 }
 
 export class GsnRefreshRelaysEvent extends GsnEvent {
-  constructor () { super('refresh-relays', 2) }
+  constructor () { super('refresh-relays', 1) }
 }
 
 export class GsnDoneRefreshRelaysEvent extends GsnEvent {
-  constructor (readonly relaysCount: number) { super('refreshed-relays', 3) }
+  constructor (readonly relaysCount: number) { super('refreshed-relays', 2) }
 }
 
 export class GsnNextRelayEvent extends GsnEvent {
-  constructor (readonly relayUrl: string) { super('next-relay', 4) }
+  constructor (readonly relayUrl: string) { super('next-relay', 3) }
 }
 
 export class GsnSignRequestEvent extends GsnEvent {
-  constructor () { super('sign-request', 5) }
+  constructor () { super('sign-request', 4) }
 }
 
 // before sending the request to the relayer, the client attempt to verify it will succeed.
 // validation may fail if the paymaster rejects the request
 export class GsnValidateRequestEvent extends GsnEvent {
-  constructor () { super('validate-request', 6) }
+  constructor () { super('validate-request', 5) }
 }
 
 export class GsnSendToRelayerEvent extends GsnEvent {
-  constructor (readonly relayUrl: string) { super('send-to-relayer', 7) }
+  constructor (readonly relayUrl: string) { super('send-to-relayer', 6) }
 }
 
 export class GsnRelayerResponseEvent extends GsnEvent {
-  constructor (readonly success: boolean) { super('relayer-response', 8) }
+  constructor (readonly success: boolean) { super('relayer-response', 7) }
 }
