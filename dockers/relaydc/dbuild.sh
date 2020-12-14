91fbeb37ef6588e19bd1b769ad3004aa991febda
Dror Tirosh <dror@opengsn.org>
2020-12-15 01:03:58 +0200

    OG-360 fix docker build scripts (#554)
    use full version string of gsnRuntimeVersion, and all docker images

--- dockers/relaydc/dbuild.sh
+++ dockers/relaydc/dbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 cd `cd \`dirname $0\`;pwd`
 
+test -z "$VERSION" && VERSION=`perl -ne "print \\$1 if /gsnRuntimeVersion.*=.*'(.*?)'/" ../../src/common/Version.ts`
+echo version=$VERSION
+
 IMAGE=opengsn/relaydc
 
 docker build -t $IMAGE .
-test -z "$VERSION" && VERSION=`perl -ne 'print $1 if /gsnRuntimeVersion.*=\D*([\d.]+)/' ../../src/common/Version.ts`
 docker tag $IMAGE $IMAGE:$VERSION
 echo "== To publish"
 echo "   docker push $IMAGE:latest; docker push $IMAGE:$VERSION"
