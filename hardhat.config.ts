8761b2f69ce851cce18c0b70e9ed6b570a29d022
shahafn <shahaflol@gmail.com>
2021-08-24 20:33:50 +0300

    solidity version in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import '@nomiclabs/hardhat-truffle5'
 import '@nomiclabs/hardhat-web3'
 
 module.exports = {
-  solidity: '0.7.3',
+  solidity: '0.8.7',
   networks: {
     hardhat: { chainId: 1337 },
     npmtest: { // used from "npm test". see package.json
