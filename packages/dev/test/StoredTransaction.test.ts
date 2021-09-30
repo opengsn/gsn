215c648d6d107bc8250ce2ae6080f7f4022230f6
shahafn <shahaflol@gmail.com>
2021-09-30 22:19:48 +0300

    flickering test

--- packages/dev/test/StoredTransaction.test.ts
+++ packages/dev/test/StoredTransaction.test.ts
@@ -8,7 +8,7 @@ import {
 import { Transaction } from '@ethereumjs/tx'
 import { bufferToHex, toBuffer } from 'ethereumjs-util'
 
-contract.only('StoredTransaction', function (accounts) {
+contract('StoredTransaction', function (accounts) {
   let tx: Transaction
   let metadata: StoredTransactionMetadata
   let serialized: StoredTransactionSerialized
