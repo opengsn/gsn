c10239c2b8802a005df18fb01be18225632ca390
shahafn <shahaflol@gmail.com>
2021-10-04 22:38:47 +0300

    circleci

--- packages/paymasters/test/HashcashPaymaster.test.ts
+++ packages/paymasters/test/HashcashPaymaster.test.ts
@@ -15,7 +15,7 @@ const HashcashPaymaster = artifacts.require('HashcashPaymaster')
 const SampleRecipient = artifacts.require('SampleRecipient')
 const IRelayHub = artifacts.require('IRelayHub')
 
-contract('HashcashPaymaster', ([from]) => {
+contract.only('HashcashPaymaster', ([from]) => {
   let pm: HashcashPaymasterInstance
   let s: SampleRecipientInstance
   let gsnConfig: Partial<GSNConfig>
