397f27a5566e8322c5475c9b57204fde564f6fc1
Dror Tirosh <dror@opengsn.org>
2021-12-21 03:34:35 +0200

    fixed deployment, verify
    yarn deploy --network name
    - deploy or update deployment  (after break in the middle or source
    modify)
    yarn verify --network name - verify contracts not yet verified.

--- packages/dev/deploy/deploy.ts
+++ packages/dev/deploy/deploy.ts
@@ -3,48 +3,63 @@ import { defaultEnvironment, environments, EnvironmentsKeys } from '@opengsn/com
 import { ethers } from 'hardhat'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { registerForwarderForGsn } from '@opengsn/common/dist/EIP712/ForwarderUtil'
+import { DeployOptions, DeployResult } from 'hardhat-deploy/dist/types'
+import chalk from 'chalk'
 
 const deploymentFunc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
+  async function deploy (name: string, options: DeployOptions): Promise<DeployResult> {
+    console.log('Deploying: ', name)
+    const res = await hre.deployments.deploy(name, options)
+    console.log(name, res.address, res.newlyDeployed === true ? 'newlyDeployed' : '')
+    return res
+  }
+
   const { deployer } = await hre.getNamedAccounts()
   console.log('deployer=', deployer)
+  const balance = await ethers.provider.getBalance(deployer)
+  if (balance.isZero()) {
+    console.error(chalk.red(`deployer account ${deployer as string} doesn't have eth balance ${balance.toString()}`))
+    process.exit(1)
+  }
   const chainId = hre.network.config.chainId
   const envname = Object.keys(environments).find(env => environments[env as EnvironmentsKeys].chainId === chainId)
   console.log('loading env', envname ?? 'DefaultEnvironment')
   const env = envname != null ? environments[envname as EnvironmentsKeys] : defaultEnvironment
 
-  console.log('using env:', env)
   const useEvents = process.env.USE_EVENTS
   if (useEvents == null) {
     console.error('must set USE_EVENTS to true/false')
+    process.exit(1)
   }
 
   const isUsingRegistryStorage = useEvents?.match(/^[TtYy1]/) != null
 
-  const deployedForwarder = await hre.deployments.deploy('Forwarder', {
+  const deployedForwarder = await deploy('Forwarder', {
     from: deployer
   })
-  if (deployedForwarder.newlyDeployed === true) {
+
+  if (deployedForwarder.newlyDeployed) {
     const f = new hre.web3.eth.Contract(deployedForwarder.abi, deployedForwarder.address)
     await registerForwarderForGsn(f, {
       from: deployer
     })
   }
 
-  const penalizer = await hre.deployments.deploy('Penalizer', {
+  const penalizer = await deploy('Penalizer', {
     from: deployer,
     args: [
       env.penalizerConfiguration.penalizeBlockDelay,
       env.penalizerConfiguration.penalizeBlockDelay
     ]
   })
-  const stakeManager = await hre.deployments.deploy('StakeManager', {
+  const stakeManager = await deploy('StakeManager', {
     from: deployer,
     args: [env.maxUnstakeDelay]
   })
 
   const hubConfig = env.relayHubConfiguration
-  const relayHub = await hre.deployments.deploy('RelayHub', {
+  const relayHub = await deploy('RelayHub', {
     from: deployer,
     args: [
       stakeManager.address,
@@ -53,14 +68,19 @@ const deploymentFunc: DeployFunction = async function (hre: HardhatRuntimeEnviro
     ]
   })
 
-  const relayRegistrar = await hre.deployments.deploy('RelayRegistrar', {
+  const relayRegistrar = await deploy('RelayRegistrar', {
     from: deployer,
     args: [relayHub.address, isUsingRegistryStorage]
   })
 
-  const hub = new ethers.Contract(relayHub.address, relayHub.abi, ethers.provider)
-  if (await hub.relayRegistrar() !== relayRegistrar.address) {
-    hub.setRegistrar(relayRegistrar.address)
+  const hub = new ethers.Contract(relayHub.address, relayHub.abi, ethers.provider.getSigner())
+  const currentRegistrar = await hub.relayRegistrar() as string
+  if (currentRegistrar !== relayRegistrar.address) {
+    if (currentRegistrar !== ethers.constants.AddressZero) {
+      console.error(chalk.red(`fatal: unable to modify registrar in hub. currently set: ${currentRegistrar}`))
+    } else {
+      await hub.setRegistrar(relayRegistrar.address).then(ret => ret.wait())
+    }
   }
 }
 
--- packages/dev/hardhat.config.ts
+++ packages/dev/hardhat.config.ts
@@ -5,9 +5,11 @@ import '@nomiclabs/hardhat-truffle5'
 import '@nomiclabs/hardhat-web3'
 import 'hardhat-deploy'
 import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
+
 import fs from 'fs'
-import { HardhatUserConfig } from "hardhat/config";
-import { NetworkUserConfig } from "hardhat/src/types/config";
+import { HardhatUserConfig } from 'hardhat/config'
+import { NetworkUserConfig } from 'hardhat/src/types/config'
 
 const mnemonicFileName = process.env.MNEMONIC_FILE
 let mnemonic = 'test '.repeat(11) + 'junk'
@@ -45,12 +47,13 @@ const config: HardhatUserConfig = {
   },
   namedAccounts: {
     deployer: 0
-  }
+  },
+  etherscan: { apiKey: process.env.ETHERSCAN_API_KEY }
 }
 
 // support url-based network: either start with "http", or a single work (infura network name)
 const neturl = process.argv.find((val, i, env) => (env[i - 1] === '--network'))
-if (neturl != null && config.networks != null) {
+if (neturl != null && config.networks != null && config.networks[neturl] == null) {
   if (neturl.match(/^http/) != null) {
     config.networks[neturl] = getNetwork(neturl)
   } else if (neturl.match(/^[\w-]+$/) != null) {
