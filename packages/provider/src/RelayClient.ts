a76d3b573bea356df479f0a16724e68ae0c543e5
Dror Tirosh <dror@opengsn.org>
2021-12-21 16:53:27 +0200

    OG-659: fix multiple sign - cont (#730)

--- packages/provider/src/RelayClient.ts
+++ packages/provider/src/RelayClient.ts
@@ -299,13 +299,21 @@ export class RelayClient {
       this.logger.info(`relayTransaction: ${JSON.stringify(httpRequest)}`)
       return { error, isRelayError: true }
     }
-    if (!this.dependencies.transactionValidator.validateRelayResponse(httpRequest, hexTransaction)) {
+    let validationError: Error|undefined
+    try {
+      if (!this.dependencies.transactionValidator.validateRelayResponse(httpRequest, hexTransaction)) {
+        validationError = new Error('Returned transaction did not pass validation')
+      }
+    } catch (e) {
+      validationError = e
+    }
+    if (validationError != null) {
       this.emit(new GsnRelayerResponseEvent(false))
       this.dependencies.knownRelaysManager.saveRelayFailure(new Date().getTime(), relayInfo.relayInfo.relayManager, relayInfo.relayInfo.relayUrl)
       return {
         auditPromise,
         isRelayError: true,
-        error: new Error('Returned transaction did not pass validation')
+        error: validationError
       }
     }
     this.emit(new GsnRelayerResponseEvent(true))
