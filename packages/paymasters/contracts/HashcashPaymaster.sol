a67b2b55066a9f8ce6dcace585b724e33ace7ca3
Dror Tirosh <dror@opengsn.org>
2022-04-12 13:59:42 +0300

    ver 3.0.0-alpha.4

--- packages/paymasters/contracts/HashcashPaymaster.sol
+++ packages/paymasters/contracts/HashcashPaymaster.sol
@@ -16,7 +16,7 @@ import "./AcceptEverythingPaymaster.sol";
 contract HashcashPaymaster is AcceptEverythingPaymaster {
 
     function versionPaymaster() external view override virtual returns (string memory){
-        return "2.2.3+opengsn.hashcash.ipaymaster";
+        return "3.0.0-alpha.4+opengsn.hashcash.ipaymaster";
     }
 
     uint8 public difficulty;
--- packages/paymasters/contracts/VerifyingPaymaster.sol
+++ packages/paymasters/contracts/VerifyingPaymaster.sol
@@ -78,7 +78,7 @@ contract VerifyingPaymaster is Ownable, BasePaymaster {
     }
 
     function versionPaymaster() external view override virtual returns (string memory){
-        return "2.2.3+opengsn.vpm.ipaymaster";
+        return "3.0.0-alpha.4+opengsn.vpm.ipaymaster";
     }
 
     function setSigner(address _signer) public onlyOwner {
