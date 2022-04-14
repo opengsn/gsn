17229fa4e79881910df621f875ab53d71a429ad7
Dror Tirosh <dror@opengsn.org>
2022-04-14 15:49:02 +0300

    deployed networks config

--- packages/dev/deployment-config.ts
+++ packages/dev/deployment-config.ts
@@ -5,7 +5,69 @@ module.exports = {
     // deploymentConfiguration is the only entry not read from the "environments"
     deploymentConfiguration: {
       paymasterDeposit: '0.1',
+      minimumStakePerToken: {
+        test: '0.1',
+        '0x610178dA211FEF7D417bC0e6FeD39F05609AD788': '0.3'
+      }
+    }
+  },
+  5: {
+    relayHubConfiguration: {
+      devAddress: '0xd21934eD8eAf27a67f0A70042Af50A1D6d195E81'
+    },
+    maxUnstakeDelay: 100000000,
+    abandonmentDelay: 10000,
+    escheatmentDelay: 500,
+    nonZeroDevFeeGasOverhead: 5596,
+    deploymentConfiguration: {
+      paymasterDeposit: '0.1',
+      deployTestPaymaster: true,
       minimumStakePerToken: { test: '0.5' }
+    }
+  },
+
+  80001: {
+    relayHubConfiguration: {
+      devAddress: '0xd21934eD8eAf27a67f0A70042Af50A1D6d195E81'
+    },
+    maxUnstakeDelay: 100000000,
+    abandonmentDelay: 10000,
+    escheatmentDelay: 500,
+    nonZeroDevFeeGasOverhead: 5596,
+    deploymentConfiguration: {
+      paymasterDeposit: '0.1',
+      deployTestPaymaster: true,
+      minimumStakePerToken: { test: '0.5' }
+    }
+  },
+
+  69: {
+    relayHubConfiguration: {
+      devAddress: '0xd21934eD8eAf27a67f0A70042Af50A1D6d195E81'
+    },
+    maxUnstakeDelay: 100000000,
+    abandonmentDelay: 10000,
+    escheatmentDelay: 500,
+    nonZeroDevFeeGasOverhead: 5596,
+    deploymentConfiguration: {
+      paymasterDeposit: '0.1',
+      deployTestPaymaster: true,
+      minimumStakePerToken: { test: '0.5' }
+    }
+  },
+
+  43113: {
+    relayHubConfiguration: {
+      devAddress: '0xd21934eD8eAf27a67f0A70042Af50A1D6d195E81'
+    },
+    maxUnstakeDelay: 100000000,
+    abandonmentDelay: 10000,
+    escheatmentDelay: 500,
+    nonZeroDevFeeGasOverhead: 5596,
+    deploymentConfiguration: {
+      paymasterDeposit: '0.1',
+      deployTestPaymaster: true,
+      minimumStakePerToken: { test: '0.1' }
     }
   }
 }
