14a5915b6cb61a5a432d44f2fc63a8afb3495033
Dror Tirosh <dror@opengsn.org>
2022-04-17 23:12:38 +0300

    OG-784: support ethers
    - add test using Ethers
    - add support for getTransactionByHash

---
+++ packages/dev/test/Ethers.test.ts
@@ -0,0 +1,75 @@
+import { GsnTestEnvironment } from '@opengsn/cli/dist/GsnTestEnvironment'
+import { RelayProvider } from '@opengsn/provider'
+import { Contract, providers, ContractFactory } from 'ethers'
+import 'source-map-support/register'
+import { expectEvent, expectRevert } from '@openzeppelin/test-helpers'
+
+const Web3HttpProvider = require('web3-providers-http')
+
+const TestRecipient = require('../../cli/src/compiled/TestRecipient.json')
+
+let NOLOG = true
+
+function logProvider (provider: any): any {
+  return {
+    send: (sendParams: any, callback: any) => {
+      const { method, params } = sendParams
+      if (NOLOG || method.match(/eth_getBlockByNumber|eth_call|eth_chainId|net_version/) != null) {
+        return provider.send(sendParams, callback)
+      }
+      console.log('>>> ', method, params)
+      provider.send(sendParams, (err: any, ret: any) => {
+        console.log('<<< ', method, err, ret)
+        callback(err, ret)
+      })
+    }
+  }
+}
+
+describe('Ethers client', () => {
+  let ethersProvider: providers.Web3Provider
+  let gsnRecipient: Contract
+  let sender: string
+
+  before(async function () {
+    this.timeout(30000)
+    const rawProvider = web3.currentProvider as any
+    console.log('host=', rawProvider.host, rawProvider.url)
+    const env = await GsnTestEnvironment.startGsn(rawProvider.host)
+    const web3provider = new Web3HttpProvider(rawProvider.host)
+
+    const { paymasterAddress, forwarderAddress } = env.contractsDeployment
+    const gsnConfig = {
+      paymasterAddress
+      // loggerConfiguration: { logLevel: 'error' }
+    }
+    const rawEthersProvider = new providers.Web3Provider((web3provider))
+    const gsnProvider = await RelayProvider.newProvider({
+      provider: web3provider,
+      config: gsnConfig
+    }).init()
+    ethersProvider = new providers.Web3Provider(logProvider(gsnProvider))
+    sender = gsnProvider.newAccount().address
+
+    const recipient = await new ContractFactory(TestRecipient.abi, TestRecipient.bytecode, rawEthersProvider.getSigner()).deploy(forwarderAddress)
+    gsnRecipient = recipient.connect(ethersProvider.getSigner(sender))
+
+    console.log('test deployed at', recipient.address)
+  })
+  after(async () => {
+    await GsnTestEnvironment.stopGsn()
+  })
+
+  it('should run command', async function () {
+    this.timeout(20000)
+    const ret = await gsnRecipient.emitMessage('hello', { gasPrice: 1e9 })
+    const rcpt = await ret.wait()
+    expectEvent.inLogs(rcpt.events, 'SampleRecipientEmitted', { realSender: sender })
+  })
+  it('should throw if target transaction fails', async function () {
+    this.timeout(20000)
+    // must pass gasLimit, to force revert on chain (off-chain revert is handled before reaching GSN)
+    await expectRevert(
+      gsnRecipient.testRevert({ gasLimit: 1e6, gasPrice: 1e9 }).then((ret: any) => ret.wait()), 'transaction failed')
+  })
+})
