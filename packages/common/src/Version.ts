ea6f2ee8bbd87d4a21aeec2564afd8fe128bd381
Dror Tirosh <dror@opengsn.org>
2022-06-26 17:01:30 +0300

    re-lint

--- packages/common/src/Version.ts
+++ packages/common/src/Version.ts
@@ -1,2 +1,2 @@
-export const gsnRuntimeVersion = require( '../package.json').version as string
+export const gsnRuntimeVersion: string = require('../package.json').version
 export const gsnRequiredVersion = '^3.0.0-alpha.4'
