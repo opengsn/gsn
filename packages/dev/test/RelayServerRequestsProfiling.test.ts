53d940331e2b7c180e15a522ad60a305a06e375d
Alex Forshtat <forshtat1@gmail.com>
2022-07-05 20:13:22 +0200

    We do make an RPC call now

--- packages/dev/test/RelayServerRequestsProfiling.test.ts
+++ packages/dev/test/RelayServerRequestsProfiling.test.ts
@@ -8,7 +8,7 @@ import { createServerLogger } from '@opengsn/relay/dist/ServerWinstonLogger'
 
 contract('RelayServerRequestsProfiling', function (accounts) {
   const refreshStateTimeoutBlocks = 2
-  const callsPerStateRefresh = 16
+  const callsPerStateRefresh = 17
   const callsPerBlock = 0
   const callsPerTransaction = 12
 
