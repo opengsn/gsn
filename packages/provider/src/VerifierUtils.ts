6994ef645a865bfa7382bfb1f6f8b0e38efcf0fe
Alex Forshtat <forshtat1@gmail.com>
2022-11-25 20:13:12 +0200

    OG-973: Implement default 'ApprovalDataCallback' for the Verifier Tool

---
+++ packages/provider/src/VerifierUtils.ts
@@ -0,0 +1,39 @@
+import { PrefixedHexString } from 'ethereumjs-util'
+import { ApprovalDataCallback, HttpWrapper, LoggerInterface, RelayRequest } from '@opengsn/common'
+
+// TODO: replace with production URL before release
+export const DEFAULT_VERIFIER_SERVER_URL = 'https://staging-api.opengsn.org'
+export const DEFAULT_VERIFIER_SERVER_APPROVAL_DATA_LENGTH = 65
+
+export interface ApprovalRequest {
+  apiKey: string
+  chainId: number
+  domainSeparatorName: string
+  relayRequest: RelayRequest
+  relayRequestId: string
+}
+
+export function createVerifierApprovalDataCallback (
+  httpWrapper: HttpWrapper,
+  logger: LoggerInterface,
+  domainSeparatorName: string,
+  chainId: number,
+  apiKey: string,
+  verifierUrl: string
+): ApprovalDataCallback {
+  return async function defaultVerifierApprovalDataCallback (
+    relayRequest: RelayRequest,
+    relayRequestId: PrefixedHexString
+  ) {
+    const approvalRequest: ApprovalRequest = {
+      apiKey,
+      chainId,
+      domainSeparatorName,
+      relayRequest,
+      relayRequestId
+    }
+    const signRelayRequestResponse = await httpWrapper.sendPromise(`${verifierUrl}/signRelayRequest`, approvalRequest)
+    logger.info(`signRelayRequest response: ${JSON.stringify(signRelayRequestResponse)}`)
+    return signRelayRequestResponse
+  }
+}
