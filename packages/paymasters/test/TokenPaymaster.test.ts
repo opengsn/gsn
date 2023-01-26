27465ba16c5078885ec250b3a5256be7940d041c
Alexhandru <alextitieni@yahoo.com>
2023-01-26 14:38:31 +0200

    TokenPayMaster - update err msg in test

--- packages/paymasters/test/TokenPaymaster.test.ts
+++ packages/paymasters/test/TokenPaymaster.test.ts
@@ -148,7 +148,7 @@ contract('TokenPaymaster', ([from, relay, relayOwner, nonUniswap, burnAddress]) 
       it('should reject if unknown paymasterData', async () => {
         const req = mergeRelayRequest(relayRequest, { paymasterData: '0x1234' })
         const signature = await getEip712Signature(web3, new TypedRequestData(defaultGsnConfig.domainSeparatorName, 1, forwarder.address, req))
-        assert.match(await revertReason(testHub.callPreRC(req, signature, '0x', 1e6)), /paymasterData: invalid length for Uniswap v1 exchange address/)
+        assert.match(await revertReason(testHub.callPreRC(req, signature, '0x', 1e6)), /paymasterData: invalid length for Uniswap v3 exchange address/)
       })
 
       it('should reject if unsupported uniswap in paymasterData', async () => {
