3a057b8698ffab8629c04b9a337083e2d9190b53
Alex Forshtat <forshtat1@gmail.com>
2023-02-28 16:59:36 +0200

    This line supposedly works on both Mac and Linux

--- packages/contracts/replaceWeb3.sh
+++ packages/contracts/replaceWeb3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -ex
-find ./types/truffle-contracts -type f -name '*.d.ts' | xargs sed -i '' 's/web3-eth-contract/..\/Web3Types/g'
+find ./types/truffle-contracts -type f -name '*.d.ts' | xargs sed -i'' -e 's/web3-eth-contract/..\/Web3Types/g'
