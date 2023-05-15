ebc7bea488d831a851c11df9b3902b3a1ec43a4f
Alex Forshtat <forshtat1@gmail.com>
2023-05-15 19:41:47 +0300

    Fix 'index.ts'

--- packages/paymasters/src/index.ts
+++ packages/paymasters/src/index.ts
@@ -1,6 +1,5 @@
 export * from './HashCashApproval'
 export * from './PermitPaymasterUtils'
-export * from './ProxyRelayProvider'
 export * from './TokenPaymasterInteractor'
 export * from './TokenPaymasterProvider'
 export * from './VerifyingPaymasterUtils'
