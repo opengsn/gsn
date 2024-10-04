3d3b9cd466c2c68b5d832ba7650514b0f5e80282
Alex Forshtat <forshtat1@gmail.com>
2024-10-05 01:35:37 +0200

    Prototype 'ForwarderLiteAccount' contract

---
+++ packages/contracts/src/forwarder/ForwarderLiteAccount.sol
@@ -0,0 +1,117 @@
+// solhint-disable not-rely-on-time
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.25;
+pragma abicoder v2;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+/**
+ * @title The Forwarder Lite Account Implementation.
+ * @notice This implementation of an EIP-7702 delegated EOAs uses ERC-712 signatures and stored nonces for verification.
+ * @notice It is intended to be used with a simplified meta-transactions relaying system.
+ * @notice It is not a full-fledged ERC-4337 style Account Abstraction account.
+ */
+contract ForwarderLiteAccount is ERC165 {
+    using ECDSA for bytes32;
+
+    address private constant DRY_RUN_ADDRESS = 0x0000000000000000000000000000000000000000;
+
+    string public constant GENERIC_PARAMS = "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data,uint256 validAfterTime,uint256 validUntilTime)";
+
+    string public constant EIP712_DOMAIN_TYPE = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+
+    uint256 private nonces;
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    function getNonce(address from)
+    public view override
+    returns (uint256) {
+        return nonces[from];
+    }
+
+    constructor() {
+        string memory requestType = string(abi.encodePacked("(", GENERIC_PARAMS, ")"));
+        registerRequestTypeInternal(requestType);
+    }
+
+    /// @inheritdoc IERC165
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC165) returns (bool) {
+        return interfaceId == type(IForwarder).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function execute(
+        ForwardRequest calldata req,
+        bytes32 domainSeparator,
+        bytes32 requestTypeHash,
+        bytes calldata suffixData,
+        bytes calldata sig
+    )
+    external payable
+    override
+    returns (bool success, bytes memory ret) {
+        _verifySig(req, domainSeparator, requestTypeHash, suffixData, sig);
+        _verifyAndUpdateNonce(req);
+
+        require(req.validUntilTime == 0 || req.validUntilTime > block.timestamp, "FWD: request expired");
+
+        bytes memory callData = abi.encodePacked(req.data, req.from);
+        require(gasleft()*63/64 >= req.gas, "FWD: insufficient gas");
+        // solhint-disable-next-line avoid-low-level-calls
+        (success,ret) = req.to.call{gas : req.gas, value : req.value}(callData);
+
+        return (success,ret);
+    }
+
+    function _verifyAndUpdateNonce(ForwardRequest calldata req) internal {
+        require(nonces[req.from]++ == req.nonce, "FWD: nonce mismatch");
+    }
+
+    function _verifySig(
+        ForwardRequest calldata req,
+        bytes calldata sig
+    )
+    internal
+    virtual
+    view
+    {
+        bytes32 digest = keccak256(abi.encodePacked(
+                "\x19\x01", domainSeparator,
+                keccak256(_getEncoded(req, requestTypeHash, suffixData))
+            ));
+        // solhint-disable-next-line avoid-tx-origin
+        require(tx.origin == DRY_RUN_ADDRESS || digest.recover(sig) == req.from, "FWD: signature mismatch");
+    }
+
+    /**
+     * @notice Creates a byte array that is a valid ABI encoding of a request of a `RequestType` type. See `execute()`.
+     */
+    function _getEncoded(
+        ForwardRequest calldata req,
+        bytes32 requestTypeHash,
+        bytes calldata suffixData
+    )
+    public
+    pure
+    returns (
+        bytes memory
+    ) {
+        // we use encodePacked since we append suffixData as-is, not as dynamic param.
+        // still, we must make sure all first params are encoded as abi.encode()
+        // would encode them - as 256-bit-wide params.
+        return abi.encodePacked(
+            requestTypeHash,
+            uint256(uint160(req.from)),
+            uint256(uint160(req.to)),
+            req.value,
+            req.gas,
+            req.nonce,
+            keccak256(req.data),
+            req.validUntilTime,
+            suffixData
+        );
+    }
+}
